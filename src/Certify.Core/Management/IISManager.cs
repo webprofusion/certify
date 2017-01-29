da38c8e443b113fadefb01e2dbd2f7a4d509974b
Christopher Cook <christopher.cook@webprofusion.com>
2017-01-29 20:23:51 +0800

    package updates

--- src/Certify.Core/Management/IISManager.cs
+++ src/Certify.Core/Management/IISManager.cs
@@ -2,7 +2,6 @@ using Microsoft.Web.Administration;
 using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
