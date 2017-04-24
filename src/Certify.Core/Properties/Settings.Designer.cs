c3c42d2c6b476007e15795e1f963e1af7a6b5892
Christopher Cook <christopher.cook@webprofusion.com>
2017-04-24 19:25:34 +0800

    Settings updates

--- src/Certify.Core/Properties/Settings.Designer.cs
+++ src/Certify.Core/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Certify.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.1.0.0")]
     public sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -25,7 +25,7 @@ namespace Certify.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("C:\\CertifyVault")]
+        [global::System.Configuration.DefaultSettingValueAttribute("C:\\ProgramData\\ACMESharp")]
         public string VaultPath {
             get {
                 return ((string)(this["VaultPath"]));
@@ -49,7 +49,7 @@ namespace Certify.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("C:\\CertifyVault")]
+        [global::System.Configuration.DefaultSettingValueAttribute("C:\\ProgramData\\ACMESharp")]
         public string DefaultVaultPath {
             get {
                 return ((string)(this["DefaultVaultPath"]));
--- src/Certify.Core/Settings.cs
+++ src/Certify.Core/Settings.cs
@@ -1,4 +1,4 @@
-﻿namespace Certify.Core.Properties
+﻿namespace Certify.Properties
 {
     // This class allows you to handle specific events on the settings class:
     //  The SettingChanging event is raised before a setting's value is changed.
