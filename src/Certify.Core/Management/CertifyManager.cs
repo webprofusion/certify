c2a759e01254199348b139db888de8b9e45df1f3
Christopher Cook <christopher.cook@webprofusion.com>
2017-04-24 19:24:38 +0800

    Task scheduling UI and updates

--- src/Certify.Core/Management/CertifyManager.cs
+++ src/Certify.Core/Management/CertifyManager.cs
@@ -324,7 +324,7 @@ namespace Certify.Management
                             var iisManager = new IISManager();
 
                             //Install certificate into certificate store and bind to IIS site
-                            if (iisManager.InstallCertForDomain(config.PrimaryDomain, pfxPath, cleanupCertStore: true, skipBindings: !config.PerformAutomatedCertBinding))
+                            if (iisManager.InstallCertForRequest(managedSite.RequestConfig, pfxPath, cleanupCertStore: true))
                             {
                                 //all done
                                 ManagedSiteLog.AppendLog(managedSite.Id, new ManagedSiteLogItem { EventDate = DateTime.UtcNow, LogItemType = LogItemType.CertificateRequestSuccessful, Message = "Completed certificate request and automated bindings update (IIS)" });
@@ -506,24 +506,48 @@ namespace Certify.Management
             }
         }
 
-        public bool CreateWindowsScheduledTask(string userId = null, string pwd = null)
+        public bool IsWindowsScheduledTaskPresent()
+        {
+            var taskList = Microsoft.Win32.TaskScheduler.TaskService.Instance.RootFolder.GetTasks();
+            if (taskList.Any(t => t.Name == SCHEDULED_TASK_NAME))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Creates the windows scheduled task to perform renewals, running as the given userid (who should be admin level so they can perform cert mgmt and IIS management functions)
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="pwd"></param>
+        /// <returns></returns>
+        public bool CreateWindowsScheduledTask(string userId, string pwd)
         {
             // https://taskscheduler.codeplex.com/documentation
-
+            var taskService = Microsoft.Win32.TaskScheduler.TaskService.Instance;
             try
             {
                 var cliPath = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, SCHEDULED_TASK_EXE);
 
-                Microsoft.Win32.TaskScheduler.TaskService.Instance.AddTask(
-                    SCHEDULED_TASK_NAME,
-                    new Microsoft.Win32.TaskScheduler.DailyTrigger { DaysInterval = 2 },
-                    new Microsoft.Win32.TaskScheduler.ExecAction(cliPath, SCHEDULED_TASK_ARGS)
-                    );
+                //setup auto renewal task, executing as admin using the given username and password
+                var task = taskService.NewTask();
+
+                task.Principal.RunLevel = Microsoft.Win32.TaskScheduler.TaskRunLevel.Highest;
+                task.Actions.Add(new Microsoft.Win32.TaskScheduler.ExecAction(cliPath, SCHEDULED_TASK_ARGS));
+                task.Triggers.Add(new Microsoft.Win32.TaskScheduler.DailyTrigger { DaysInterval = 1 });
+
+                //register/update task
+                taskService.RootFolder.RegisterTaskDefinition(SCHEDULED_TASK_NAME, task, Microsoft.Win32.TaskScheduler.TaskCreation.CreateOrUpdate, userId, pwd, Microsoft.Win32.TaskScheduler.TaskLogonType.Password);
 
                 return true;
             }
-            catch (Exception)
+            catch (Exception exp)
             {
+                System.Diagnostics.Debug.WriteLine(exp.ToString());
                 //failed to create task
                 return false;
             }
---
+++ src/Certify.UI/Windows/ScheduledTaskConfig.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Certify.UI.Windows
+{
+    /// <summary>
+    /// Interaction logic for ScheduledTaskConfig.xaml
+    /// </summary>
+    public partial class ScheduledTaskConfig
+    {
+        public ScheduledTaskConfig()
+        {
+            InitializeComponent();
+        }
+
+        private void OK_Click(object sender, RoutedEventArgs e)
+        {
+            //create/update scheduled task
+            if (!String.IsNullOrEmpty(Username.Text) && (!String.IsNullOrEmpty(Password.Password)))
+            {
+                var certifyManager = new Certify.Management.CertifyManager();
+                if (certifyManager.CreateWindowsScheduledTask(Username.Text, Password.Password))
+                {
+                    MessageBox.Show("Scheduled task created");
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Failed to create scheduled task with given credentials");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please provide the username and password for an admin level user.");
+            }
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
