bd94310144da5cb4685627c2502aaf34926937ff
webprofusion <christopher.cook@webprofusion.com>
2017-10-06 10:49:32 +0800

    Add Unit Tests project, add test for renewal due checks

---
+++ src/Certify.Tests/Certify.Core.Tests.Unit/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Certify.Core.Tests.Unit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Certify.Core.Tests.Unit")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c0534cd8-10e9-438d-b9a1-a8c09a6bb964")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/Certify.Tests/Certify.Core.Tests.Unit/SettingsTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace Certify.Core.Tests.Unit
+{
+    [TestClass]
+    public class SettingsTest
+    {
+        [TestMethod, Description("Ensure a site which should be renewed correctly requires renewal")]
+        public void TestCheckAutoRenewalPeriodRequired()
+        {
+            // setup
+            var renewalPeriodDays = 14;
+            var managedSite = new Models.ManagedSite { IncludeInAutoRenew = true, DateRenewed = DateTime.Now.AddDays(-15), DateExpiry = DateTime.Now.AddDays(60) };
+
+            // perform check
+            var isRenewalRequired = Management.CertifyManager.IsRenewalRequired(managedSite, renewalPeriodDays);
+
+            // assert result
+            Assert.IsTrue(isRenewalRequired, "Renewal should be required");
+        }
+
+        [TestMethod, Description("Ensure a site which should not be renewed correctly does not require renewal")]
+        public void TestCheckAutoRenewalPeriodNotRequired()
+        {
+            // setup : set renewal period to 30 days, last renewal 15 days ago. Renewal should not be
+            // required yet.
+            var renewalPeriodDays = 30;
+            var managedSite = new Models.ManagedSite { IncludeInAutoRenew = true, DateRenewed = DateTime.Now.AddDays(-15), DateExpiry = DateTime.Now.AddDays(60) };
+
+            // perform check
+            var isRenewalRequired = Management.CertifyManager.IsRenewalRequired(managedSite, renewalPeriodDays);
+
+            // assert result
+            Assert.IsFalse(isRenewalRequired, "Renewal should not be required");
+        }
+
+        [TestMethod, Description("Ensure a site with unknown date for last renewal should require renewal")]
+        public void TestCheckAutoRenewalPeriodUnknownLastRenewal()
+        {
+            // setup : set renewal period to 14 days, last renewal unknown.
+
+            var renewalPeriodDays = 14;
+            var managedSite = new Models.ManagedSite { IncludeInAutoRenew = true, DateExpiry = DateTime.Now.AddDays(60) };
+
+            // perform check
+            var isRenewalRequired = Management.CertifyManager.IsRenewalRequired(managedSite, renewalPeriodDays);
+
+            // assert result
+            Assert.IsTrue(isRenewalRequired, "Renewal should be required");
+        }
+    }
+}
