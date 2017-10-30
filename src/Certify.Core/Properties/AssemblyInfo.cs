cde80e9fced224dc672a5c07e576349a626a24a8
webprofusion <christopher.cook@webprofusion.com>
2017-10-30 17:03:05 +0800

    Update version

--- src/Certify.Core/Properties/AssemblyInfo.cs
+++ src/Certify.Core/Properties/AssemblyInfo.cs
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers by using the '*'
 // as shown below: [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.21.*")]
+[assembly: AssemblyVersion("2.1.22.*")]
 [assembly: AssemblyFileVersion("2.0.0")]
