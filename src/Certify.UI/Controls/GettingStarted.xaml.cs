84b84cc3f55436c4bdd11747a55906dd2e860f0d
webprofusion <christopher.cook@webprofusion.com>
2017-11-23 20:54:02 +0800

    Split  Getting Started UI into new control

---
+++ src/Certify.UI/Controls/GettingStarted.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Certify.UI.Controls
+{
+    /// <summary>
+    /// Interaction logic for GettingStarted.xaml 
+    /// </summary>
+    public partial class GettingStarted : UserControl
+    {
+        public GettingStarted()
+        {
+            InitializeComponent();
+        }
+
+        private void AddToDashboard_Click(object sender, RoutedEventArgs e)
+        {
+            var d = new Windows.AddToDashboard { Owner = Window.GetWindow(this) };
+            d.ShowDialog();
+        }
+
+        private void ViewDashboard_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://certifytheweb.com/dashboard");
+        }
+    }
+}
