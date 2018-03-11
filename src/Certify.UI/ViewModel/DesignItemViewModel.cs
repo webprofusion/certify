26e10c5d9d9ee9d1c3b07cc56a6bdbff0e121dc7
webprofusion <christopher.cook@webprofusion.com>
2018-03-11 18:21:13 +0800

    Show date of last renewal attempt in status info

--- src/Certify.UI/ViewModel/DesignItemViewModel.cs
+++ src/Certify.UI/ViewModel/DesignItemViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Certify.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -23,6 +24,7 @@ namespace Certify.UI
 
                 SelectedItem.RenewalFailureCount = 3;
                 SelectedItem.RenewalFailureMessage = "This is an example renewal failure message. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+                SelectedItem.DateLastRenewalAttempt = DateTime.Now.AddMinutes(-30);
             }
         }
 
