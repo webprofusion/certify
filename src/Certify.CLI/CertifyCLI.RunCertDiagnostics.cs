acb84fc0be3131037b9402bde72ad5902a325a15
webprofusion <christopher.cook@webprofusion.com>
2018-06-04 20:05:35 +0800

    CLI re-org

---
+++ src/Certify.CLI/CertifyCLI.RunCertDiagnostics.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Certify.Management;
+using Certify.Models;
+
+namespace Certify.CLI
+{
+    public partial class CertifyCLI
+    {
+        public void RunCertDiagnostics()
+        {
+            var managedCertificates = _certifyClient.GetManagedCertificates(new ManagedCertificateFilter()).Result;
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine("Checking existing bindings..");
+
+            var bindingConfig = Certify.Utils.Networking.GetCertificateBindings().Where(b => b.Port == 443);
+
+            foreach (var b in bindingConfig)
+            {
+                Console.WriteLine($"{b.IP}:{b.Port}");
+            }
+
+            var dupeBindings = bindingConfig.GroupBy(x => x.IP + ":" + x.Port)
+              .Where(g => g.Count() > 1)
+              .Select(y => y.Key)
+              .ToList();
+
+            if (dupeBindings.Any())
+            {
+                foreach (var d in dupeBindings)
+                {
+                    Console.WriteLine($"Duplicate binding will fail:  {d}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No duplicate IP:Port bindings identified.");
+            }
+
+            Console.WriteLine("Running cert diagnostics..");
+
+            foreach (var site in managedCertificates)
+            {
+                if (!String.IsNullOrEmpty(site.CertificatePath))
+                {
+                    if (System.IO.File.Exists(site.CertificatePath))
+                    {
+                        Console.WriteLine($"{site.Name}");
+                        var fileCert = CertificateManager.LoadCertificate(site.CertificatePath);
+
+                        if (fileCert != null)
+                        {
+                            try
+                            {
+                                var storedCert = CertificateManager.GetCertificateFromStore(site.RequestConfig.PrimaryDomain);
+                                if (storedCert != null)
+                                {
+                                    Console.WriteLine($"Stored cert :: " + storedCert.FriendlyName);
+                                    var test = fileCert.PrivateKey.KeyExchangeAlgorithm;
+                                    Console.WriteLine(test.ToString());
+
+                                    var access = CertificateManager.GetUserAccessInfoForCertificatePrivateKey(storedCert);
+                                    foreach (System.Security.AccessControl.AuthorizationRule a in access.GetAccessRules(true, false, typeof(System.Security.Principal.NTAccount)))
+                                    {
+                                        Console.WriteLine("\t Access: " + a.IdentityReference.Value.ToString());
+                                    }
+                                }
+                                else
+                                {
+                                    Console.WriteLine($"{site.RequestConfig.PrimaryDomain} :: Stored cert not found");
+                                }
+                            }
+                            catch (Exception exp)
+                            {
+                                Console.WriteLine(exp.ToString());
+                            }
+                        }
+                    }
+                    else
+                    {
+                        //Console.WriteLine($"{site.Name} certificate file does not exist: {site.CertificatePath}");
+                    }
+                }
+            }
+
+            Console.WriteLine("-----------");
+        }
+    }
+}
---
+++ src/Certify.CLI/CertifyCLI.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Certify.Client;
+using Certify.Management;
+using Certify.Models;
+using Microsoft.ApplicationInsights;
+
+namespace Certify.CLI
+{
+    public partial class CertifyCLI
+    {
+        private TelemetryClient _tc = null;
+        private ICertifyClient _certifyClient = null;
+        private Preferences _prefs = new Preferences();
+        private PluginManager _pluginManager { get; set; }
+
+        public CertifyCLI()
+        {
+            _certifyClient = new CertifyServiceClient();
+        }
+
+        public async Task<bool> IsServiceAvailable()
+        {
+            bool isAvailable = false;
+
+            try
+            {
+                await _certifyClient.GetAppVersion();
+                isAvailable = true;
+            }
+            catch (Exception)
+            {
+                isAvailable = false;
+            }
+            return isAvailable;
+        }
+
+        private void InitPlugins()
+        {
+            _pluginManager = new Management.PluginManager();
+
+            _pluginManager.LoadPlugins();
+        }
+
+        private bool IsRegistered()
+        {
+            var licensingManager = _pluginManager.LicensingManager;
+            if (licensingManager != null)
+            {
+                if (licensingManager.IsInstallRegistered(1, Certify.Management.Util.GetAppDataFolder()))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public async Task LoadPreferences()
+        {
+            _prefs = await _certifyClient.GetPreferences();
+        }
+
+        private bool IsTelematicsEnabled()
+        {
+            return _prefs.EnableAppTelematics;
+        }
+
+        private string GetInstrumentationKey()
+        {
+            return Certify.Locales.ConfigResources.AIInstrumentationKey;
+        }
+
+        private async Task<string> GetAppVersion()
+        {
+            try
+            {
+                return await _certifyClient.GetAppVersion();
+            }
+            catch (Exception)
+            {
+                return await Task.FromResult("--- (Service Not Started)");
+            }
+        }
+
+        private string GetAppWebsiteURL()
+        {
+            return Certify.Locales.ConfigResources.AppWebsiteURL;
+        }
+
+        private void InitTelematics()
+        {
+            if (IsTelematicsEnabled())
+            {
+                _tc = new TelemetryClient();
+                _tc.Context.InstrumentationKey = GetInstrumentationKey();
+                _tc.InstrumentationKey = GetInstrumentationKey();
+
+                // Set session data:
+
+                _tc.Context.Session.Id = Guid.NewGuid().ToString();
+                _tc.Context.Component.Version = GetAppVersion().Result;
+                _tc.Context.Device.OperatingSystem = Environment.OSVersion.ToString();
+                _tc.TrackEvent("StartCLI");
+            }
+        }
+
+        internal void ShowVersion()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            System.Console.WriteLine("Certify SSL Manager - CLI v4.0.0. Certify.Core v" + GetAppVersion().Result);
+            Console.ForegroundColor = ConsoleColor.White;
+            System.Console.WriteLine("For more information see " + GetAppWebsiteURL());
+            System.Console.WriteLine("");
+        }
+
+        internal void ShowACMEInfo()
+        {
+            /*
+                        var certifyManager = new CertifyManager();
+                        string vaultInfo = certifyManager.GetVaultSummary();
+                        string acmeInfo = certifyManager.GetAcmeSummary();
+
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+                        System.Console.WriteLine("Let's Encrypt ACME API: " + acmeInfo);
+                        System.Console.WriteLine("ACMESharp Vault: " + vaultInfo);
+            */
+            System.Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        internal void ShowHelp()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            System.Console.WriteLine("Usage: certify <command> \n");
+            System.Console.WriteLine("certify renew : renew certificates for all auto renewed managed sites");
+            System.Console.WriteLine("certify list : list managed certificates and current running/not running status in IIS");
+            System.Console.WriteLine("certify diag : check existing ssl bindings and managed certificate integrity");
+            System.Console.WriteLine("certify importcsv : import managed certificates from a CSV file.");
+            System.Console.WriteLine("\n\n");
+            System.Console.WriteLine("\n\n");
+            System.Console.WriteLine("For help, see the docs at https://docs.certifytheweb.com");
+
+            System.Console.WriteLine("\n");
+        }
+
+        internal async Task PerformAutoRenew()
+        {
+            if (_tc == null) InitTelematics();
+            if (_tc != null)
+            {
+                _tc.TrackEvent("CLI_BeginAutoRenew");
+            }
+
+            Console.ForegroundColor = ConsoleColor.White;
+            System.Console.WriteLine("\nPerforming Auto Renewals..\n");
+
+            //go through list of items configured for auto renew, perform renewal and report the result
+            var results = await _certifyClient.BeginAutoRenewal();
+            Console.ForegroundColor = ConsoleColor.White;
+
+            foreach (var r in results)
+            {
+                if (r.ManagedItem != null)
+                {
+                    System.Console.WriteLine("--------------------------------------");
+                    if (r.IsSuccess)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        System.Console.WriteLine(r.ManagedItem.Name);
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+                        System.Console.WriteLine(r.ManagedItem.Name);
+
+                        if (r.Message != null)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            System.Console.WriteLine(r.Message);
+                        }
+                    }
+                }
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+
+            System.Console.WriteLine("Completed:" + results.Where(r => r.IsSuccess == true).Count());
+            if (results.Any(r => r.IsSuccess == false))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                System.Console.WriteLine("Failed:" + results.Where(r => r.IsSuccess == false).Count());
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+
+        internal void ListManagedCertificates()
+        {
+            var managedCertificates = _certifyClient.GetManagedCertificates(new ManagedCertificateFilter()).Result;
+
+            foreach (var site in managedCertificates)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+
+                Console.WriteLine($"{site.Name},{site.DateExpiry}");
+            }
+        }
+    }
+}
