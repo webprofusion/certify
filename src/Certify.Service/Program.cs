32978f725bd00a527d4f19c2766444023116c165
webprofusion <christopher.cook@webprofusion.com>
2018-07-13 08:52:32 +0800

    Check status reporting pref before reporting service exception

--- src/Certify.Service/Program.cs
+++ src/Certify.Service/Program.cs
@@ -1,8 +1,8 @@
-﻿using Certify.Locales;
-using Microsoft.Owin.Hosting;
-using System;
+﻿using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Certify.Locales;
+using Microsoft.Owin.Hosting;
 using Topshelf;
 
 namespace Certify.Service
@@ -51,39 +51,41 @@ namespace Certify.Service
 
             if (e.ExceptionObject != null)
             {
-                //submit diagnostic info if connection available
+                //submit diagnostic info if connection available and status reporting enabled
+                if (Management.CoreAppSettings.Current.EnableStatusReporting)
+                {
+                    var API_BASE_URI = Locales.ConfigResources.APIBaseURI;
 
-                var API_BASE_URI = Locales.ConfigResources.APIBaseURI;
+                    var client = new HttpClient();
 
-                var client = new HttpClient();
-
-                var jsonRequest = Newtonsoft.Json.JsonConvert.SerializeObject(
-                    new Models.Shared.FeedbackReport
-                    {
-                        EmailAddress = "(service exception)",
-                        Comment = "An unhandled service exception has occurred.: " + ((Exception)e.ExceptionObject).ToString(),
-                        IsException = true,
-                        AppVersion = ConfigResources.AppName + " " + new Certify.Management.Util().GetAppVersion(),
-                        SupportingData = new
+                    var jsonRequest = Newtonsoft.Json.JsonConvert.SerializeObject(
+                        new Models.Shared.FeedbackReport
                         {
-                            Framework = Certify.Management.Util.GetDotNetVersion(),
-                            OS = Environment.OSVersion.ToString(),
+                            EmailAddress = "(service exception)",
+                            Comment = "An unhandled service exception has occurred.: " + ((Exception)e.ExceptionObject).ToString(),
+                            IsException = true,
                             AppVersion = ConfigResources.AppName + " " + new Certify.Management.Util().GetAppVersion(),
-                            IsException = true
-                        }
-                    });
+                            SupportingData = new
+                            {
+                                Framework = Certify.Management.Util.GetDotNetVersion(),
+                                OS = Environment.OSVersion.ToString(),
+                                AppVersion = ConfigResources.AppName + " " + new Certify.Management.Util().GetAppVersion(),
+                                IsException = true
+                            }
+                        });
 
-                var data = new StringContent(jsonRequest, System.Text.Encoding.UTF8, "application/json");
-                try
-                {
-                    Task.Run(async () =>
+                    var data = new StringContent(jsonRequest, System.Text.Encoding.UTF8, "application/json");
+                    try
                     {
-                        await client.PostAsync(API_BASE_URI + "feedback/submit", data);
-                    });
-                }
-                catch (Exception exp)
-                {
-                    System.Diagnostics.Debug.WriteLine(exp.ToString());
+                        Task.Run(async () =>
+                        {
+                            await client.PostAsync(API_BASE_URI + "feedback/submit", data);
+                        });
+                    }
+                    catch (Exception exp)
+                    {
+                        System.Diagnostics.Debug.WriteLine(exp.ToString());
+                    }
                 }
             }
         }
@@ -109,8 +111,8 @@ namespace Certify.Service
                 // failed to listen on service uri, attempt reconfiguration of port.
                 int currentPort = serviceConfig.Port;
 
-                int newPort = currentPort+=2;
-                
+                int newPort = currentPort += 2;
+
                 serviceUri = $"http://{serviceConfig.Host}:{newPort}";
                 _webApp = WebApp.Start<APIHost>(serviceUri);
 
@@ -123,7 +125,7 @@ namespace Certify.Service
 
         public void Stop()
         {
-            if (_webApp!=null) _webApp.Dispose();
+            if (_webApp != null) _webApp.Dispose();
         }
     }
 }
