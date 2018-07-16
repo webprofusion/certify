44d735d962cd61cf17a682b2268dab78cd616e95
webprofusion <christopher.cook@webprofusion.com>
2018-07-16 17:05:56 +0800

    WIP: support for buypass ACME CA

--- src/Certify.Models/CertificateAuthority.cs
+++ src/Certify.Models/CertificateAuthority.cs
@@ -1,31 +1,65 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace Certify.Models
 {
-    public enum CertAuthorityType
+    public enum CertAuthorityAPIType
     {
-        LetsEncrypt
+        NONE,
+        ACME_V1,
+        ACME_V2
+    }
+
+    public enum CertAuthoritySupportedRequests
+    {
+        DOMAIN_SINGLE,
+        DOMAIN_MULTIPLE_SAN,
+        DOMAIN_WILDCARD
     }
 
     public class CertificateAuthority
     {
         public static List<CertificateAuthority> CertificateAuthorities = new List<CertificateAuthority> {
             new CertificateAuthority{
-                Id=CertAuthorityType.LetsEncrypt,
+                Id="letsencrypt.org",
                 Title ="Let's Encrypt",
-                WebsiteURL ="https://letsencrypt.org/",
-                PrivacyPolicyURL ="https://letsencrypt.org/privacy/"
+                APIType = CertAuthorityAPIType.ACME_V2,
+                WebsiteUrl ="https://letsencrypt.org/",
+                PrivacyPolicyUrl ="https://letsencrypt.org/privacy/",
+                TermsAndConditionsUrl="https://letsencrypt.org/repository/",
+                ProductionAPIEndpoint = "https://acme-v02.api.letsencrypt.org/directory",
+                StagingAPIEndpoint = "https://acme-staging-v02.api.letsencrypt.org/directory",
+                IsEnabled = true,
+                SupportedRequests = new List<CertAuthoritySupportedRequests>{
+                    CertAuthoritySupportedRequests.DOMAIN_SINGLE,
+                    CertAuthoritySupportedRequests.DOMAIN_MULTIPLE_SAN,
+                    CertAuthoritySupportedRequests.DOMAIN_WILDCARD
+                }
+            },
+             new CertificateAuthority{
+                Id="buypass.com",
+                Title ="Let's Encrypt",
+                APIType = CertAuthorityAPIType.ACME_V1,
+                WebsiteUrl ="https://www.buypass.com/",
+                PrivacyPolicyUrl ="https://www.buypass.com/about-buypass/privacy-policy",
+                ProductionAPIEndpoint = "https://api.buypass.com/acme/directory",
+                StagingAPIEndpoint = null,
+                IsEnabled=false,
+                 SupportedRequests = new List<CertAuthoritySupportedRequests>{
+                    CertAuthoritySupportedRequests.DOMAIN_SINGLE
+                }
             }
         };
 
-        public CertAuthorityType Id { get; set; }
+        public string Id { get; set; }
+        public CertAuthorityAPIType APIType { get; set; }
+        public List<CertAuthoritySupportedRequests> SupportedRequests { get; set; }
         public string Title { get; set; }
-
-        public string WebsiteURL { get; set; }
-        public string PrivacyPolicyURL { get; set; }
+        public string Decription { get; set; }
+        public string WebsiteUrl { get; set; }
+        public string PrivacyPolicyUrl { get; set; }
+        public string TermsAndConditionsUrl { get; set; }
+        public string ProductionAPIEndpoint { get; set; }
+        public string StagingAPIEndpoint { get; set; }
+        public bool IsEnabled { get; set; }
     }
 }
