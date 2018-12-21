1a21caf9fadef06438ff8dfd585e7b9e5c0ee6f9
AJ Henderson <ahenderson@autotask.com>
2018-12-21 16:04:54 -0500

    Provider Parameter List and Password Support

--- src/Certify.UI/Windows/EditCredential.xaml.cs
+++ src/Certify.UI/Windows/EditCredential.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 using Certify.Models.Config;
 
@@ -13,6 +14,155 @@ namespace Certify.UI.Windows
         public ObservableCollection<ProviderParameter> CredentialSet { get; set; }
         public StoredCredential Item { get; set; }
         public List<ProviderDefinition> ChallengeProviders { get; set; }
+    }
+    public static class PasswordBoxAssistant
+    {
+        public static readonly DependencyProperty BoundPassword =
+          DependencyProperty.RegisterAttached("BoundPassword", typeof(string), typeof(PasswordBoxAssistant), new PropertyMetadata(string.Empty, OnBoundPasswordChanged));
+
+        public static readonly DependencyProperty BindPassword = DependencyProperty.RegisterAttached(
+            "BindPassword", typeof(bool), typeof(PasswordBoxAssistant), new PropertyMetadata(false, OnBindPasswordChanged));
+
+        private static readonly DependencyProperty UpdatingPassword =
+            DependencyProperty.RegisterAttached("UpdatingPassword", typeof(bool), typeof(PasswordBoxAssistant), new PropertyMetadata(false));
+
+        private static void OnBoundPasswordChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var box = d as PasswordBox;
+
+            // only handle this event when the property is attached to a PasswordBox
+            // and when the BindPassword attached property has been set to true
+            if (d == null || !GetBindPassword(d))
+            {
+                return;
+            }
+
+            // avoid recursive updating by ignoring the box's changed event
+            box.PasswordChanged -= HandlePasswordChanged;
+
+            var newPassword = (string)e.NewValue;
+
+            if (!GetUpdatingPassword(box))
+            {
+                box.Password = newPassword;
+            }
+
+            box.PasswordChanged += HandlePasswordChanged;
+        }
+
+        private static void OnBindPasswordChanged(DependencyObject dp, DependencyPropertyChangedEventArgs e)
+        {
+            // when the BindPassword attached property is set on a PasswordBox,
+            // start listening to its PasswordChanged event
+
+            var box = dp as PasswordBox;
+
+            if (box == null)
+            {
+                return;
+            }
+
+            var wasBound = (bool)(e.OldValue);
+            var needToBind = (bool)(e.NewValue);
+
+            if (wasBound)
+            {
+                box.PasswordChanged -= HandlePasswordChanged;
+            }
+
+            if (needToBind)
+            {
+                box.PasswordChanged += HandlePasswordChanged;
+            }
+        }
+
+        private static void HandlePasswordChanged(object sender, RoutedEventArgs e)
+        {
+            var box = sender as PasswordBox;
+
+            // set a flag to indicate that we're updating the password
+            SetUpdatingPassword(box, true);
+            // push the new password into the BoundPassword property
+            SetBoundPassword(box, box.Password);
+            SetUpdatingPassword(box, false);
+        }
+
+        public static void SetBindPassword(DependencyObject dp, bool value)
+        {
+            dp.SetValue(BindPassword, value);
+        }
+
+        public static bool GetBindPassword(DependencyObject dp)
+        {
+            return (bool)dp.GetValue(BindPassword);
+        }
+
+        public static string GetBoundPassword(DependencyObject dp)
+        {
+            return (string)dp.GetValue(BoundPassword);
+        }
+
+        public static void SetBoundPassword(DependencyObject dp, string value)
+        {
+            dp.SetValue(BoundPassword, value);
+        }
+
+        private static bool GetUpdatingPassword(DependencyObject dp)
+        {
+            return (bool)dp.GetValue(UpdatingPassword);
+        }
+
+        private static void SetUpdatingPassword(DependencyObject dp, bool value)
+        {
+            dp.SetValue(UpdatingPassword, value);
+        }
+    }
+
+    /// <summary>
+    /// Selects template based on the type of the data item. 
+    /// </summary>
+    public class ControlTemplateSelector : DataTemplateSelector
+    {
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            var context = container as FrameworkElement;
+            DataTemplate template = null;
+
+            if (null == container)
+            {
+                throw new NullReferenceException("container");
+            }
+            else if (null == context)
+            {
+                throw new Exception("container must be FramekworkElement");
+            }
+            else if (null == item)
+            {
+                return null;
+            }
+
+            template = null;
+
+            var providerParameter = item as ProviderParameter;
+            if (providerParameter == null)
+            {
+                template = context.FindResource("ProviderStringParameter") as DataTemplate;
+            }
+            else if (providerParameter.IsPassword)
+            {
+                template = context.FindResource("ProviderPasswordParameter") as DataTemplate;
+            }
+            else if (providerParameter.Options.Count() != 0)
+            {
+                template = context.FindResource("ProviderDropDownParameter") as DataTemplate;
+            }
+            else
+            {
+                template = context.FindResource("ProviderStringParameter") as DataTemplate;
+            }
+
+            return template ?? base.SelectTemplate(item, container);
+        }
     }
 
     /// <summary>
@@ -40,7 +190,7 @@ namespace Certify.UI.Windows
         {
             InitializeComponent();
 
-            this.DataContext = EditViewModel;
+            DataContext = EditViewModel;
 
             // TODO: move to async
             if (MainViewModel.ChallengeAPIProviders == null && MainViewModel.ChallengeAPIProviders.Count == 0)
@@ -50,8 +200,8 @@ namespace Certify.UI.Windows
 
             EditViewModel.ChallengeProviders = MainViewModel
                 .ChallengeAPIProviders
-                .Where(p => p.ProviderParameters.Any(pa=>pa.IsCredential))
-                .OrderBy(p=>p.Title)
+                .Where(p => p.ProviderParameters.Any(pa => pa.IsCredential))
+                .OrderBy(p => p.Title)
                 .ToList();
 
             if (editItem != null)
@@ -67,16 +217,16 @@ namespace Certify.UI.Windows
                 };
             }
 
-            this.RefreshCredentialOptions();
+            RefreshCredentialOptions();
         }
 
         private async void Save_Click(object sender, System.Windows.RoutedEventArgs e)
         {
             StoredCredential credential;
 
-            Dictionary<String, string> credentialsToStore = new Dictionary<string, string>();
+            var credentialsToStore = new Dictionary<string, string>();
 
-            if (String.IsNullOrEmpty(EditViewModel.Item.Title))
+            if (string.IsNullOrEmpty(EditViewModel.Item.Title))
             {
                 MessageBox.Show("Stored credentials require a name.");
                 return;
@@ -88,17 +238,17 @@ namespace Certify.UI.Windows
                 return;
             }
 
-            foreach (var c in this.EditViewModel.CredentialSet)
+            foreach (var c in EditViewModel.CredentialSet)
             {
                 //store entered value
 
-                if (c.IsRequired && String.IsNullOrEmpty(c.Value))
+                if (c.IsRequired && string.IsNullOrEmpty(c.Value))
                 {
                     MessageBox.Show($"{c.Name} is a required value");
                     return;
                 }
 
-                if (!String.IsNullOrEmpty(c.Value))
+                if (!string.IsNullOrEmpty(c.Value))
                 {
                     credentialsToStore.Add(c.Key, c.Value);
                 }
@@ -130,9 +280,9 @@ namespace Certify.UI.Windows
                 };
             }
 
-            this.EditViewModel.Item = await MainViewModel.UpdateCredential(credential);
+            EditViewModel.Item = await MainViewModel.UpdateCredential(credential);
 
-            this.Close();
+            Close();
         }
 
         private void CredentialTypes_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
@@ -140,22 +290,22 @@ namespace Certify.UI.Windows
             // populate credentials list
             if (e.Source != null)
             {
-                this.RefreshCredentialOptions();
+                RefreshCredentialOptions();
             }
         }
 
         private void RefreshCredentialOptions()
         {
-            var selectedType = this.ProviderTypes.SelectedItem as ProviderDefinition;
+            var selectedType = ProviderTypes.SelectedItem as ProviderDefinition;
             if (selectedType != null)
             {
-                this.EditViewModel.CredentialSet = new ObservableCollection<ProviderParameter>(selectedType.ProviderParameters.Where(p=>p.IsCredential));
+                EditViewModel.CredentialSet = new ObservableCollection<ProviderParameter>(selectedType.ProviderParameters.Where(p => p.IsCredential));
             }
         }
 
         private void Cancel_Click(object sender, System.Windows.RoutedEventArgs e)
         {
-            this.Close();
+            Close();
         }
 
         private async void Test_Click(object sender, RoutedEventArgs e)
