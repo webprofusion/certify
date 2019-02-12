31d5e792ee3c6ff4182122465ad9262876221a46
PromontoryProtean <john@cybertechnical.com>
2019-02-12 09:13:43 -0500

    Added support for saving the Certify app window width, height and position when closing the main app window. The settings are stored in a file called ui.json in the program data directory, and the settings are read when the app window is loaded to restore the previous session window size and position.

---
+++ src/Certify.UI/Settings/UISettings.cs
@@ -0,0 +1,11 @@
+﻿namespace Certify.UI.Settings
+{
+    class UISettings
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public double Left { get; set; }
+        public double Top { get; set; }
+    }
+}
