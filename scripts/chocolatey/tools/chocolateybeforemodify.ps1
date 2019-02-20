50317c090c8a40294538f0849158f87a0c5e5bd6
webprofusion-chrisc <christopher.cook@webprofusion.com>
2019-02-20 18:57:48 +0800

    Add chocolatey package nuspec

---
+++ scripts/chocolatey/tools/chocolateybeforemodify.ps1
@@ -0,0 +1,14 @@
+﻿# This runs in 0.9.10+ before upgrade and uninstall.
+# Use this file to do things like stop services prior to upgrade or uninstall.
+# NOTE: It is an anti-pattern to call chocolateyUninstall.ps1 from here. If you
+#  need to uninstall an MSI prior to upgrade, put the functionality in this
+#  file without calling the uninstall script. Make it idempotent in the
+#  uninstall script so that it doesn't fail when it is already uninstalled.
+# NOTE: For upgrades - like the uninstall script, this script always runs from 
+#  the currently installed version, not from the new upgraded package version.
+
+# Close the UI window if currently open
+Get-Process | Where-Object {$_.ProcessName -eq 'Certify.UI'} | Foreach-Object { $_.CloseMainWindow() | Out-Null } | stop-process –force
+
+# Stop the Certify.Service background service
+Get-Service Certify.Service | Where {$_.status –eq 'Running'} |  Stop-Service
---
+++ scripts/chocolatey/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url64      = 'https://certifytheweb.s3.amazonaws.com/downloads/CertifyTheWebSetup_SilentInstall.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url64bit      = $url64
+  softwareName  = 'Certify The Web*'
+  checksum64    = '38b03e226f3f5270f9eb17e19a9def092c81220ae87a77569a1b2f57c8645485'
+  checksumType64= 'sha256'
+  validExitCodes= @(0)
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+}
+
+Install-ChocolateyPackage @packageArgs
