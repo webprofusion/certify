b598a52ecbf5303c79a722a3ad3cbf4883ef0037
webprofusion-chrisc <christopher.cook@webprofusion.com>
2020-02-05 16:58:30 +0800

    Update app version (5.0)

--- src/Certify.UI/Properties/AssemblyInfo.cs
+++ src/Certify.UI/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Certify SSL Manager")]
-[assembly: AssemblyCopyright("Copyright © Webprofusion Pty Ltd 2019")]
+[assembly: AssemblyCopyright("Copyright © Webprofusion Pty Ltd 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -41,5 +41,5 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers by using the '*'
 // as shown below: [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.0.0.*")]
-[assembly: AssemblyFileVersion("4.0.0.0")]
+[assembly: AssemblyVersion("5.0.0.*")]
+[assembly: AssemblyFileVersion("5.0.0.0")]
