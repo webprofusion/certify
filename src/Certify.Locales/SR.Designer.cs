65914a680c27b9b538e1566304dc9a7f93e9f577
webprofusion-chrisc <christopher.cook@webprofusion.com>
2020-02-14 11:23:34 +0800

    Version number updates

--- src/Certify.Locales/SR.Designer.cs
+++ src/Certify.Locales/SR.Designer.cs
@@ -19,7 +19,7 @@ namespace Certify.Locales {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class SR {
@@ -655,7 +655,7 @@ namespace Certify.Locales {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to The certificate file for this item has not been created yet..
+        ///   Looks up a localized string similar to The certificate file for this item has not been created yet or the file has been deleted..
         /// </summary>
         public static string ManagedCertificateSettings_CertificateNotReady {
             get {
