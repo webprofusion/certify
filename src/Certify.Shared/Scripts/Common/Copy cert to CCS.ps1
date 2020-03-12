3ef1665d8c53ad199445a3d671f0afae96848c71
webprofusion-chrisc <christopher.cook@webprofusion.com>
2020-03-12 13:33:51 +0800

    More example scripts

---
+++ src/Certify.Shared/Scripts/Common/Copy cert to CCS.ps1
@@ -0,0 +1,19 @@
+﻿# example script to copy the output PFX to a UNC path for Central Certificate Store
+# Enabling CCS: https://techcommunity.microsoft.com/t5/iis-support-blog/central-certificate-store-ccs-with-iis/ba-p/377274
+$result = Get-Content -Raw -Path C:\temp\ps-test.json | ConvertFrom-Json
+
+$CcsPath="\\wbp-desktop06\ccs"
+$CcsServer="wbp-desktop06"
+
+if ($result.IsSuccess -eq $true) {
+
+    # connect network share with credentials
+    net use $CcsPath /USER:wbp-desktop06\testuser password1
+
+    # example file copy where cert contains webmail.example.com and autodiscover.example.com domains
+    Copy-Item -Path $result.ManagedItem.CertificatePath -Destination $CcsPath\webmail.example.com.pfx -Force
+    Copy-Item -Path $result.ManagedItem.CertificatePath -Destination $CcsPath\autodiscover.example.com.pfx -Force
+
+    # disconnect network share
+    net use $CcsPath /delete
+}
---
+++ src/Certify.Shared/Scripts/Common/Export PFX to Apache.ps1
@@ -0,0 +1,28 @@
+﻿# This is an example script and it will be overwritten when the next update is installed. 
+# To use this script copy it to another location and modify as required
+
+param($result)
+
+# script example adapted from https://community.certifytheweb.com/t/filezilla-server-ps-script/141/8
+
+# Alias to your OpenSSL install
+set-alias opensslcmd "C:\Tools\OpenSSL\openssl.exe" 
+
+# Set paths to where keys will be saved. 
+# This will vary depending on your required configuration. File name are not that important but your config must point to the same filenames.
+
+$keypath = "C:\apache\conf\mydomain.com\"
+$key = $keypath + "cert.key"
+$rsakey = $keypath + "cert_rsa.key"
+$pem = $keypath + "cert.pem"
+
+# Get the latest PFX file path
+$sourcepfx = $result.ManagedItem.CertificatePath
+
+# Create the Key, RSA Key, and PEM file. Use the RSA Key & PEM for FileZilla
+opensslcmd pkcs12 -in $sourcepfx -out $key -nocerts -nodes -passin pass:
+opensslcmd rsa -in $key -out $rsakey
+opensslcmd pkcs12 -in $sourcepfx -out $pem -nokeys -clcerts -passin pass:
+
+# optional: restart the Apache service (example)
+Restart-Service -Name Apache2.4 -Force
