01d90c556ec3377c86fd3b94850adc3b4e9279cb
webprofusion-chrisc <christopher.cook@webprofusion.com>
2020-05-11 16:25:24 +0800

    WIP: CA editor

--- src/Certify.Models/CertificateAuthority.cs
+++ src/Certify.Models/CertificateAuthority.cs
@@ -38,6 +38,7 @@ namespace Certify.Models
                 ProductionAPIEndpoint = "https://acme-v02.api.letsencrypt.org/directory",
                 StagingAPIEndpoint = "https://acme-staging-v02.api.letsencrypt.org/directory",
                 IsEnabled = true,
+                IsCustom = false,
                 SANLimit=100,
                 RequiresEmailAddress = true,
                 SupportedFeatures = new List<string>{
@@ -49,25 +50,25 @@ namespace Certify.Models
              new CertificateAuthority{
                 Id="buypass.com",
                 Title ="Buypass Go SSL",
-                Description="Buypass Go SSL is a free SSL certificate service from Buypass CA using the Buypass ACME API. Certificates are valid for 180 days and can be for one domain wildcard or contain 1 (one) domain and one www. subdomain (optional).",
+                Description="Buypass Go SSL is a free SSL certificate service from Buypass CA using the Buypass ACME API. Certificates are valid for 180 days and can be for one domain and a www. subdomain (optional).",
                 APIType = CertAuthorityAPIType.ACME_V2.ToString(),
                 WebsiteUrl ="https://www.buypass.com/",
                 PrivacyPolicyUrl ="https://www.buypass.com/about-buypass/privacy-policy",
                 ProductionAPIEndpoint = "https://api.buypass.com/acme/directory",
                 StagingAPIEndpoint = "https://api.test4.buypass.no/acme/directory",
                 IsEnabled=true,
+                IsCustom = false,
                 SANLimit=1,
                 RequiresEmailAddress = true,
                 SupportedFeatures = new List<string>{
                     CertAuthoritySupportedRequests.DOMAIN_SINGLE.ToString(),
-                    CertAuthoritySupportedRequests.DOMAIN_SINGLE_PLUS_WWW.ToString(),
-                    CertAuthoritySupportedRequests.DOMAIN_WILDCARD.ToString()
+                    CertAuthoritySupportedRequests.DOMAIN_SINGLE_PLUS_WWW.ToString()
                 }
             }
         };
 
         public string Id { get; set; }
-        public string APIType { get; set; }
+        public string APIType { get; set; } = CertAuthorityAPIType.ACME_V2.ToString();
         public List<string> SupportedFeatures { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
@@ -77,6 +78,7 @@ namespace Certify.Models
         public string ProductionAPIEndpoint { get; set; }
         public string StagingAPIEndpoint { get; set; }
         public bool IsEnabled { get; set; }
+        public bool IsCustom { get; set; } = true;
         public int SANLimit { get; set; }
         public bool RequiresEmailAddress { get; set; }
         public bool AllowUntrustedTls { get; set; } = false;
---
+++ src/Certify.UI/Windows/EditCertificateAuthority.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Input;
+using Certify.Models;
+
+namespace Certify.UI.Windows
+{
+    /// <summary>
+    /// Edit details for an ACME Account
+    /// </summary>
+    public partial class EditCertificateAuthority
+    {
+        public CertificateAuthority Item { get; set; }
+
+        public Certify.UI.ViewModel.AppViewModel MainViewModel => ViewModel.AppViewModel.Current;
+
+        public IEnumerable<CertificateAuthority> CertificateAuthorities => MainViewModel.CertificateAuthorities;
+
+        public EditCertificateAuthority()
+        {
+            InitializeComponent();
+
+            Item = new CertificateAuthority();
+
+            DataContext = this;
+
+            this.Width *= MainViewModel.UIScaleFactor;
+            this.Height *= MainViewModel.UIScaleFactor;
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            Mouse.OverrideCursor = Cursors.Arrow;
+            Close();
+        }
+
+        private async void Save_Click(object sender, RoutedEventArgs e)
+        {
+            //add/update contact
+
+          //  var ca = MainViewModel.CertificateAuthorities.FirstOrDefault(c => c.Id == Item.CertificateAuthorityId);
+
+            // if ca requires email address, check that first
+           /* if (ca.RequiresEmailAddress)
+            {
+                var isValidEmail = true;
+                if (string.IsNullOrEmpty(Item.EmailAddress))
+                {
+                    isValidEmail = false;
+                }
+                else
+                {
+                    if (!Regex.IsMatch(Item.EmailAddress,
+                                @"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
+                                @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$",
+                                RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250)))
+                    {
+                        isValidEmail = false;
+                    }
+                }
+
+                if (!isValidEmail)
+                {
+                    MessageBox.Show(Certify.Locales.SR.New_Contact_EmailError);
+
+                    return;
+                }
+            }
+
+            if (Item.AgreedToTermsAndConditions)
+            {
+                Mouse.OverrideCursor = Cursors.Wait;
+
+                var result = await MainViewModel.AddContactRegistration(Item);
+
+                Mouse.OverrideCursor = Cursors.Arrow;
+
+                if (result.IsSuccess)
+                {
+                    await MainViewModel.RefreshAccountsList();
+
+                    Close();
+                }
+                else
+                {
+                    MessageBox.Show(result.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show(Certify.Locales.SR.New_Contact_NeedAgree);
+            }*/
+        }
+
+        private void CertificateAuthorityList_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
