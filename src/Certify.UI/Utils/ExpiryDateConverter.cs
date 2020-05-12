60eb0014f29273d332f141641d2a5f7f9b1034a6
webprofusion-chrisc <christopher.cook@webprofusion.com>
2020-05-12 14:37:19 +0800

    Fix text colour for certs not yet requested

--- src/Certify.UI/Utils/ExpiryDateConverter.cs
+++ src/Certify.UI/Utils/ExpiryDateConverter.cs
@@ -42,11 +42,6 @@ namespace Certify.UI.Utils
     {
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
-            if (value == null)
-            {
-                return DependencyProperty.UnsetValue;
-            }
-
             return GetColour((DateTime?)value);
         }
 
