80bed8f8d42292ef1a979cb9e551a40fb42ab3b9
webprofusion-chrisc <christopher.cook@webprofusion.com>
2020-05-25 15:39:13 +0800

    Minor refactoring

---
+++ src/Certify.Shared/Utils/Loggy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Serilog;
+
+namespace Certify.Models
+{
+    public class Loggy : Providers.ILog
+    {
+        private ILogger _log;
+
+        public Loggy(ILogger log)
+        {
+            _log = log;
+        }
+
+        public void Error(string template, params object[] propertyValues) => _log.Error(template, propertyValues);
+
+        public void Error(Exception exp, string template, params object[] propertyValues) => _log.Error(exp, template, propertyValues);
+
+        public void Information(string template, params object[] propertyValues) => _log.Information(template, propertyValues);
+
+        public void Debug(string template, params object[] propertyValues) => _log.Debug(template, propertyValues);
+
+        public void Verbose(string template, params object[] propertyValues) => _log.Verbose(template, propertyValues);
+
+        public void Warning(string template, params object[] propertyValues) => _log.Warning(template, propertyValues);
+    }
+}
--- src/Certify.Shared/Utils/ManagedCertificateLog.cs
+++ src/Certify.Shared/Utils/ManagedCertificateLog.cs
@@ -16,28 +16,6 @@ namespace Certify.Models
         CertficateRequestAttentionRequired = 110
     }
 
-    public class Loggy : Providers.ILog
-    {
-        private ILogger _log;
-
-        public Loggy(ILogger log)
-        {
-            _log = log;
-        }
-
-        public void Error(string template, params object[] propertyValues) => _log.Error(template, propertyValues);
-
-        public void Error(Exception exp, string template, params object[] propertyValues) => _log.Error(exp, template, propertyValues);
-
-        public void Information(string template, params object[] propertyValues) => _log.Information(template, propertyValues);
-
-        public void Debug(string template, params object[] propertyValues) => _log.Debug(template, propertyValues);
-
-        public void Verbose(string template, params object[] propertyValues) => _log.Verbose(template, propertyValues);
-
-        public void Warning(string template, params object[] propertyValues) => _log.Warning(template, propertyValues);
-    }
-
     public class ManagedCertificateLogItem
     {
         public DateTime EventDate { get; set; }
