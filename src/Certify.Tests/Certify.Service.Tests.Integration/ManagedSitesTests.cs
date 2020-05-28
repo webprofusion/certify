b2f7411f3aaaa0ab170c4c376a94263f6722e4d1
webprofusion-chrisc <christopher.cook@webprofusion.com>
2020-05-28 13:56:02 +0800

    Add standard demo sites setup as a test

--- src/Certify.Tests/Certify.Service.Tests.Integration/ManagedSitesTests.cs
+++ src/Certify.Tests/Certify.Service.Tests.Integration/ManagedSitesTests.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Threading.Tasks;
 using Certify.Models;
+using System.Collections.Generic;
 
 namespace Certify.Service.Tests.Integration
 {
@@ -29,6 +30,47 @@ namespace Certify.Service.Tests.Integration
             //attempt to get single item
             var site = await _client.GetManagedCertificate(results[0].Id);
             Assert.IsNotNull(site, $"Fetched single managed site details");
+        }
+
+
+        [TestMethod]
+        [Ignore]
+        public async Task TestCreateManagedCertificates()
+        {
+            //get full list
+
+            var list = new List<ManagedCertificate>
+            {
+                GetExample("msdn.webprofusion.com", 12),
+                GetExample("demo.webprofusion.co.uk", 45),
+                GetExample("clients.dependencymanager.com",75),
+                GetExample("soundshed.com",32),
+                GetExample("*.projectbids.co.uk",48),
+                GetExample("exchange.projectbids.co.uk",19),
+                GetExample("remote.dependencymanager.com",7),
+                GetExample("git.example.com",56)
+
+            };
+
+            foreach (var site in list)
+            {
+
+                await _client.UpdateManagedCertificate(site);
+            }
+            
+        }
+
+        private ManagedCertificate GetExample(string title, int numDays)
+        {
+            return new ManagedCertificate()
+            {
+                Id = Guid.NewGuid().ToString(),
+                Name = title,
+                DateExpiry = DateTime.Now.AddDays(numDays),
+                DateLastRenewalAttempt = DateTime.Now.AddDays(-1),
+                LastRenewalStatus = RequestState.Success
+
+            };
         }
     }
 }
