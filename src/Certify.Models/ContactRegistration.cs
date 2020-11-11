ec347b801148072927a94b9accb08951f7885f3e
webprofusion-chrisc <christopher.cook@webprofusion.com>
2020-11-11 17:07:14 +0800

    UI updates

--- src/Certify.Models/ContactRegistration.cs
+++ src/Certify.Models/ContactRegistration.cs
@@ -8,5 +8,10 @@
 
         public string CertificateAuthorityId { get; set; } = StandardCertAuthorities.LETS_ENCRYPT;
         public bool IsStaging { get; set; } = false;
+
+        public string EabKeyId { get; set; }
+        public string EabKey { get; set; }
+        public string EabKeyAlgorithm { get; set; }
+        public string PreferredChain { get; set; }
     }
 }
