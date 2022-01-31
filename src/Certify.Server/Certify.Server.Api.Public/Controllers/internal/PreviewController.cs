1bb923479acf7288af645407e4690a10ece06d4c
webprofusion-chrisc <christopher.cook@webprofusion.com>
2022-01-31 17:17:22 +0800

    API: preview controller

---
+++ src/Certify.Server/Certify.Server.Api.Public/Controllers/internal/PreviewController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Certify.Client;
+using Certify.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Certify.Server.API.Controllers
+{
+    /// <summary>
+    /// Internal API for extended certificate management. Not intended for general use.
+    /// </summary>
+    [ApiController]
+    [Route("internal/v1/[controller]")]
+    public class PreviewController : ControllerBase
+    {
+
+        private readonly ILogger<SystemController> _logger;
+
+        private readonly ICertifyInternalApiClient _client;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="client"></param>
+        public PreviewController(ILogger<SystemController> logger, ICertifyInternalApiClient client)
+        {
+            _logger = logger;
+            _client = client;
+        }
+
+        /// <summary>
+        /// Get preview of steps for certificate order and deployment
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<ActionStep>))]
+        public async Task<IActionResult> GetPreview([FromBody]ManagedCertificate item)
+        {
+            var previewSteps = await _client.PreviewActions(item);
+            return new OkObjectResult(previewSteps);
+        }
+
+    }
+}
