b021bedaf9376123c363b1326782ae9e2c8b19a7
webprofusion-chrisc <christopher.cook@webprofusion.com>
2022-02-02 11:35:44 +0800

    Update choco

--- scripts/chocolatey/tools/chocolateyinstall.ps1
+++ scripts/chocolatey/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64      = 'https://certifytheweb.s3.amazonaws.com/downloads/archive/CertifyTheWebSetup_V5.6.4.exe'
+$url64      = 'https://certifytheweb.s3.amazonaws.com/downloads/archive/CertifyTheWebSetup_V5.6.5.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -8,7 +8,7 @@ $packageArgs = @{
   fileType      = 'exe'
   url64bit      = $url64
   softwareName  = 'Certify The Web*'
-  checksum64    = 'b4e71d4ca4465725d0d4adee02cbf728c0ea2e61758200dbe046ba02dcd532ab'
+  checksum64    = 'ceb399b28a806d281986e8b430834053b3c960966799a33d46a13ee008411072'
   checksumType64= 'sha256'
   validExitCodes= @(0)
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
