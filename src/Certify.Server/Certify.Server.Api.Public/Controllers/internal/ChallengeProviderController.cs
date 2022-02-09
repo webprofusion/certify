76b01b221dc75e493bee7d9a3bede4dcf673ce25
webprofusion-chrisc <christopher.cook@webprofusion.com>
2022-02-09 20:31:26 +0800

    Implement api for deployment and challenge providers

---
+++ src/Certify.Server/Certify.Server.Api.Public/Controllers/internal/ChallengeProviderController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Certify.Client;
+using Certify.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Certify.Server.API.Controllers
+{
+    /// <summary>
+    /// Internal API for extended certificate management. Not intended for general use.
+    /// </summary>
+    [ApiController]
+    [Route("internal/v1/[controller]")]
+    public class ChallengeProviderController : ControllerBase
+    {
+
+        private readonly ILogger<SystemController> _logger;
+
+        private readonly ICertifyInternalApiClient _client;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="client"></param>
+        public ChallengeProviderController(ILogger<SystemController> logger, ICertifyInternalApiClient client)
+        {
+            _logger = logger;
+            _client = client;
+        }
+
+
+        /// <summary>
+        /// Get list of supported challenge providers
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<Models.Config.ChallengeProviderDefinition>))]
+
+        public async Task<IActionResult> GetChallengeProviders()
+        {
+            var list = await _client.GetChallengeAPIList();
+            return new OkObjectResult(list);
+        }
+
+
+    }
+}
