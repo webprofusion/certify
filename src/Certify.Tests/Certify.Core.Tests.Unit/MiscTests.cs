cefe4bf9f4f15f9937eccdcdaf5d47ef3fc7c27c
Christopher Cook <christopher.cook@webprofusion.com>
2022-05-30 15:36:10 +0800

    Cleanup

--- src/Certify.Tests/Certify.Core.Tests.Unit/MiscTests.cs
+++ src/Certify.Tests/Certify.Core.Tests.Unit/MiscTests.cs
@@ -14,26 +14,7 @@ namespace Certify.Core.Tests.Unit
 
         }
 
-        [TestMethod, Description("Ensure source config with nulls deserialises as empty string instead of null")]
-        public void TestLoadManagedCertificates()
-        {
-            var json = @"{ 'Id':null,'Title':null,'Description':null,'HelpUrl':'','ProviderParameters':[],'Config':null,'IsExperimental':false,'IsEnabled':true,'IsTestModeSupported':true,'HasDynamicParameters':false}";
-            json = json.Replace("'", "\"");
-
-            var provider = JsonConvert.DeserializeObject<ProviderDefinition>(json, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
-            Assert.AreEqual(String.Empty, provider.Id);
-
-            provider = JsonConvert.DeserializeObject<ProviderDefinition>(json);
-            Assert.IsNull(provider.Id);
-
-            provider = System.Text.Json.JsonSerializer.Deserialize<ProviderDefinition>(json, new System.Text.Json.JsonSerializerOptions { DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull });
-            Assert.AreEqual(String.Empty, provider.Id);
-
-            provider = System.Text.Json.JsonSerializer.Deserialize<ProviderDefinition>(json);
-            Assert.IsNull(provider.Id);
-
-        }
-
+  
         [TestMethod, Description("Test null/blank coalesce of string")]
         public void TestNullOrBlankCoalesce()
         {
