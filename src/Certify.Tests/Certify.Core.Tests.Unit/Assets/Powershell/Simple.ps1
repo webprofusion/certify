71a816501cf8a19b229f7c285768c99e1d2d1c90
webprofusion-chrisc <christopher.cook@webprofusion.com>
2023-04-12 17:55:44 +0800

    Fix test script

--- src/Certify.Tests/Certify.Core.Tests.Unit/Assets/Powershell/Simple.ps1
+++ src/Certify.Tests/Certify.Core.Tests.Unit/Assets/Powershell/Simple.ps1
@@ -6,4 +6,4 @@ Start-Transcript -Path C:\Temp\Certify\TestOutput\TestTranscript.txt
 Get-Help Get-Item
 
 Add-Content C:\Temp\Certify\TestOutput\TestPSOutput.txt "Testing 123"
-Stop=-Transcript
+Stop-Transcript
