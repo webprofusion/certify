2abbb16d858d1b72364a0b025ba85abec5888369
webprofusion-chrisc <christopher.cook@webprofusion.com>
2023-08-14 14:05:43 +0800

    RDAP: implement rdap query

---
+++ src/Certify.Shared/Utils/RdapService.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Certify.Shared.Core.Utils
+{
+    public class RdapResult
+    {
+        public string Identifier { get; set; }
+        public DateTimeOffset? DateRegistered { get; set; }
+        public DateTimeOffset? DateExpiry { get; set; }
+        public DateTimeOffset? DateLastChanged { get; set; }
+        public string[] Nameservers { get; set; }
+        public string Status { get; set; }
+        public string Error { get; set; }
+        public string RawJSON { get; set; }
+        public RdapResponse Response { get; set; }
+    }
+
+    public class RdapResponse
+    {
+        public string LdhName { get; set; }
+        public string[] Status { get; set; }
+        public RdapEvent[] Events { get; set; }
+        public RdapNameservers[] Nameservers { get; set; }
+    }
+
+    public class RdapEvent
+    {
+        public string EventAction { get; set; }
+        public DateTimeOffset EventDate { get; set; }
+    }
+
+    public class RdapNameservers
+    {
+        public string ObjectClassName { get; set; }
+        public string LdhName { get; set; }
+    }
+
+    public class RdapDnsRoot
+    {
+        public string Description { get; set; }
+        public DateTime Publication { get; set; }
+        public List<List<List<string>>> Services { get; set; }
+        public string Version { get; set; }
+    }
+
+    public class RdapService
+    {
+        private List<string> _publicSuffixList = new List<string>();
+
+        private Hashtable _dnsRootConfig = new Hashtable();
+
+        public async Task Init()
+        {
+            var list = System.IO.File.ReadAllLines("Assets\\public_suffix_list.dat");
+            foreach (var line in list)
+            {
+                if (line.StartsWith("// ===END ICANN DOMAINS==="))
+                {
+                    // we don't currently use custom domains
+                    break;
+                }
+
+                if (!string.IsNullOrWhiteSpace(line) && !line.StartsWith("//"))
+                {
+                    _publicSuffixList.Add(line);
+                }
+            }
+
+            var rdapDnsConfig = System.IO.File.ReadAllText("Assets\\rdap_dns.json");
+
+            var dnsRootConfig = JsonConvert.DeserializeObject<RdapDnsRoot>(rdapDnsConfig);
+            foreach (var svc in dnsRootConfig.Services)
+            {
+
+                foreach (var tld in svc[0])
+                {
+                    _dnsRootConfig.Add(tld, svc[1][0]);
+                }
+            }
+        }
+
+        public string GetRdapQueryURL(string tld)
+        {
+            try
+            {
+                return _dnsRootConfig[tld].ToString();
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public async Task<RdapResult> QueryRDAP(string domain)
+        {
+            // normalise domain to primary based on the tld
+            var queryDomain = NormaliseDomain(domain);
+
+            var queryTld = GetTLD(queryDomain, true);
+
+            // find which service to query
+            var queryUrl = GetRdapQueryURL(queryTld);
+
+            if (queryUrl != null)
+            {
+                // query for rdap response
+                
+                var httpClient = new HttpClient();
+                httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                try
+                {
+                    var json = await httpClient.GetStringAsync($"{queryUrl}/domain/{queryDomain}");
+                    var result = new RdapResult { Status = "OK", RawJSON = json };
+                    result.Response = JsonConvert.DeserializeObject<RdapResponse>(json);
+                    result.Identifier = result.Response.LdhName;
+                    result.Nameservers = result.Response.Nameservers.Select(n => n.LdhName).ToArray();
+                    result.DateExpiry = result.Response.Events.FirstOrDefault(e => e.EventAction == "expiration")?.EventDate;
+                    result.DateRegistered = result.Response.Events.FirstOrDefault(e => e.EventAction == "registration")?.EventDate;
+                    result.DateLastChanged = result.Response.Events.FirstOrDefault(e => e.EventAction == "last changed")?.EventDate;
+
+                    return result;
+                }
+                catch (Exception ex)
+                {
+                    // return normalised response
+                    return new RdapResult { Status = "Error", Error = ex.Message };
+                }
+                finally
+                {
+                    httpClient.Dispose();
+                }
+            }
+            else
+            {
+                return new RdapResult { Status = "Error", Error = "There is no RDAP Server available for the '{queryTld}' TLD" };
+            }
+        }
+
+        public string GetTLD(string domain, bool shortestFirst = false)
+        {
+            var tldQuery = _publicSuffixList
+                .Where(s => domain.ToLowerInvariant().EndsWith(s));
+
+            if (!shortestFirst)
+            {
+                tldQuery = tldQuery.OrderByDescending(s => s.Length);
+            }
+
+            var tld = tldQuery.FirstOrDefault();
+
+            return tld;
+        }
+
+        /// <summary>
+        /// return base domain for a given subdomain
+        /// </summary>
+        /// <param name="domain"></param>
+        /// <returns></returns>
+        public string NormaliseDomain(string domain, bool resolveToHighestLevel = false)
+        {
+
+            domain = domain.ToLower();
+
+            var tld = GetTLD(domain);
+
+            if (resolveToHighestLevel)
+            {
+                var topTld = GetTLD(tld, shortestFirst: true);
+                while (topTld != tld)
+                {
+                    tld = topTld;
+                    topTld = GetTLD(tld);
+                }
+            }
+
+            if (tld != null)
+            {
+
+                domain = domain
+                    .Substring(0, domain.LastIndexOf(tld))
+                    .Trim('.')
+                    .Split('.')
+                    .LastOrDefault();
+
+                domain = $"{domain}.{tld}";
+            }
+
+            return domain;
+        }
+    }
+}
---
+++ src/Certify.Tests/Certify.Core.Tests.Integration/RdapTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using Certify.Shared.Core.Utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Certify.Core.Tests
+{
+    [TestClass]
+    public class RdapTests
+    {
+
+        public RdapTests()
+        {
+
+        }
+
+        [TestMethod, Description("Test Rdap Query")]
+        [DataTestMethod]
+        [DataRow("example.com", "OK", null)]
+        [DataRow("www.example.com", "OK", null)]
+        [DataRow("www.example.co.uk", "Error", null)]
+        [DataRow("test.musician.io", "Error", null)]
+        [DataRow("webprofusion.com", "OK", null)]
+        public async Task TestDomainRdapQuery(string domain, string expectedStatus, DateTimeOffset? registered)
+        {
+            var rdap = new RdapService();
+
+            await rdap.Init();
+
+            var result = await rdap.QueryRDAP(domain);
+
+            Assert.IsNotNull(result, "Result expected");
+
+            Assert.AreEqual(expectedStatus, result.Status, "Status expected");
+
+            System.Diagnostics.Debug.WriteLine($"Domain: {domain}  Registered {result.DateRegistered} Updated {result.DateLastChanged} Expiration {result.DateExpiry}");
+        }
+    }
+}
