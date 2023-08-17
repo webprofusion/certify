211818fb027b37fc387efc7e8aeae732ffbcec91
webprofusion-chrisc <christopher.cook@webprofusion.com>
2023-08-17 12:32:18 +0800

    Import/Export: optionally skip deployment

--- src/Certify.Core/Management/CertifyManager/CertifyManager.cs
+++ src/Certify.Core/Management/CertifyManager/CertifyManager.cs
@@ -463,6 +463,66 @@ namespace Certify.Management
         public void Dispose() => ManagedCertificateLog.DisposeLoggers();
 
         /// <summary>
+        /// Perform (or preview) an import of settings from another instance
+        /// </summary>
+        /// <param name="importRequest"></param>
+        /// <returns></returns>
+        public async Task<List<ActionStep>> PerformImport(ImportRequest importRequest)
+        {
+            var migrationManager = new MigrationManager(_itemManager, _credentialsManager, _serverProviders);
+
+            var importResult = await migrationManager.PerformImport(importRequest.Package, importRequest.Settings, importRequest.IsPreviewMode);
+
+            // store and apply certs if we have no errors
+
+            var hasError = false;
+            if (!importResult.Any(i => i.HasError))
+            {
+                if (importRequest.Settings.IncludeDeployment)
+                {
+
+                    var deploySteps = new List<ActionStep>();
+                    foreach (var m in importRequest.Package.Content.ManagedCertificates)
+                    {
+                        var managedCert = await GetManagedCertificate(m.Id);
+
+                        if (managedCert != null && !string.IsNullOrEmpty(managedCert.CertificatePath))
+                        {
+                            var deployResult = await DeployCertificate(managedCert, null, isPreviewOnly: importRequest.IsPreviewMode);
+
+                            deploySteps.Add(new ActionStep { Category = "Deployment", HasError = !deployResult.IsSuccess, Key = managedCert.Id, Description = deployResult.Message });
+                        }
+                    }
+
+                    importResult.Add(new ActionStep { Title = "Deployment" + (importRequest.IsPreviewMode ? " [Preview]" : ""), Substeps = deploySteps });
+                }
+            }
+            else
+            {
+                hasError = true;
+            }
+
+            _tc?.TrackEvent("Import" + (importRequest.IsPreviewMode ? "_Preview" : ""), new Dictionary<string, string> {
+                { "hasErrors", hasError.ToString() }
+            });
+
+            return importResult;
+        }
+
+        /// <summary>
+        /// Perform (or preview) and export of settings from this instance
+        /// </summary>
+        /// <param name="exportRequest"></param>
+        /// <returns></returns>
+        public async Task<ImportExportPackage> PerformExport(ExportRequest exportRequest)
+        {
+            _tc?.TrackEvent("Export" + (exportRequest.IsPreviewMode ? "_Preview" : ""));
+
+            var migrationManager = new MigrationManager(_itemManager, _credentialsManager, _serverProviders);
+            return await migrationManager.PerformExport(exportRequest.Filter, exportRequest.Settings, exportRequest.IsPreviewMode);
+        }
+
+        /// <summary>
         /// Get the current service log (per line)
         /// </summary>
         /// <param name="type"></param>
