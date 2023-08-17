62ea6f39e2267cbc81add62c8561b9b24d7a06cc
webprofusion-chrisc <christopher.cook@webprofusion.com>
2023-08-17 08:32:48 +0800

    Worker service updates

--- src/Certify.Server/Certify.Service.Worker/Certify.Service.Worker/Program.cs
+++ src/Certify.Server/Certify.Service.Worker/Certify.Service.Worker/Program.cs
@@ -39,113 +39,122 @@ namespace Certify.Service.Worker
                      logging.ClearProviders();
                      logging.AddConsole();
 
-                 })
-                .UseSystemd()
-                .ConfigureServices((hostContext, services) =>
-                {
-                    if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-                    {
-                        services.AddWindowsService(options =>
-                            options.ServiceName = "Certify Certificate Manager Background Service"
+                 });
 
-                           );
-                    }
+            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                builder.UseSystemd();
+            }
 
-                    services.AddHostedService<Worker>();
-                })
-                .ConfigureWebHostDefaults(webBuilder =>
+            builder.ConfigureServices((hostContext, services) =>
+            {
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
                 {
-                    webBuilder.ConfigureKestrel(serverOptions =>
+                    services.AddWindowsService(options =>
+                        options.ServiceName = "Certify Certificate Manager Background Service"
+
+                       );
+                }
+
+                services.AddHostedService<Worker>();
+            })
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.ConfigureKestrel(serverOptions =>
+                {
+                    if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
                     {
                         serverOptions.UseSystemd();
-                        // configure https listener, cert path and pwd can come either from an environment variable, usersecrets or appsettings.
-                        // configuration precedence is secrets first, https://docs.microsoft.com/en-us/aspnet/core/fundamentals/configuration/?view=aspnetcore-5.0#default
-                        var configuration = (IConfiguration)serverOptions.ApplicationServices.GetService(typeof(IConfiguration));
+                    }
 
-                        var useHttps = bool.Parse(configuration["API:Service:UseHttps"]);
+                    // configure https listener, cert path and pwd can come either from an environment variable, usersecrets or appsettings.
+                    // configuration precedence is secrets first, https://docs.microsoft.com/en-us/aspnet/core/fundamentals/configuration/?view=aspnetcore-5.0#default
+                    var configuration = (IConfiguration)serverOptions.ApplicationServices.GetService(typeof(IConfiguration));
 
-                        // default IP to localhost then specify from configuration
-                        var ipSelection = configuration["API:Service:BindingIP"];
-                        var ipBinding = IPAddress.Loopback;
+                    var useHttps = bool.Parse(configuration["API:Service:UseHttps"]);
 
-                        if (ipSelection != null)
+                    // default IP to localhost then specify from configuration
+                    var ipSelection = configuration["API:Service:BindingIP"];
+                    var ipBinding = IPAddress.Loopback;
+
+                    if (ipSelection != null)
+                    {
+                        if (ipSelection.ToLower() == "loopback")
                         {
-                            if (ipSelection.ToLower() == "loopback")
-                            {
-                                ipBinding = IPAddress.Loopback;
-                            }
-                            else if (ipSelection.ToLower() == "any")
-                            {
-                                ipBinding = IPAddress.Any;
-                            }
-                            else
-                            {
-                                ipBinding = IPAddress.Parse(ipSelection);
-                            }
+                            ipBinding = IPAddress.Loopback;
                         }
-
-                        if (useHttps)
+                        else if (ipSelection.ToLower() == "any")
                         {
-
-                            var certPassword = Environment.GetEnvironmentVariable("ASPNETCORE_Kestrel__Certificates__Development__Password");
-                            var certPath = Environment.GetEnvironmentVariable("ASPNETCORE_Kestrel__Certificates__Development__Path");
-
-                            // if not yet defined load config from usersecrets (development env only) or appsettings
-                            if (certPassword == null)
-                            {
-                                certPassword = configuration["Kestrel:Certificates:Default:Password"];
-                            }
-
-                            if (certPath == null)
-                            {
-                                certPath = configuration["Kestrel:Certificates:Default:Path"];
-                            }
-
-                            try
-                            {
-                                var certificate = new X509Certificate2(certPath, certPassword);
-
-                                // if password is wrong at this stage the attempts to use the cert will results in SSL Protocol Error
-
-                                var httpsConnectionAdapterOptions = new HttpsConnectionAdapterOptions()
-                                {
-                                    ClientCertificateMode = ClientCertificateMode.NoCertificate,
-                                    SslProtocols = System.Security.Authentication.SslProtocols.Tls12 | System.Security.Authentication.SslProtocols.Tls13,
-                                    ServerCertificate = certificate,
-                                };
-
-                                var httpsPort = Convert.ToInt32(configuration["API:Service:HttpsPort"]);
-
-                                serverOptions.Listen(new System.Net.IPEndPoint(ipBinding, httpsPort), listenOptions =>
-                                {
-                                    listenOptions.UseHttps(httpsConnectionAdapterOptions);
-                                });
-
-                            }
-                            catch (Exception exp)
-                            {
-                                // TODO: there is no logger yet, need to report this failure to main log once the log exists
-                                System.Diagnostics.Debug.WriteLine("Failed to load PFX certificate for application. Check service certificate config." + exp.ToString());
-                            }
+                            ipBinding = IPAddress.Any;
                         }
                         else
                         {
-                            var httpPort = Convert.ToInt32(configuration["API:Service:HttpPort"]);
-
-                            serverOptions.Listen(new System.Net.IPEndPoint(ipBinding, httpPort), listenOptions =>
-                            {
-                            });
+                            ipBinding = IPAddress.Parse(ipSelection);
                         }
-                    });
+                    }
 
-                    webBuilder.ConfigureLogging(logging =>
-                      {
-                          logging.AddFilter("Microsoft.AspNetCore.SignalR", LogLevel.Debug);
-                          logging.AddFilter("Microsoft.AspNetCore.Http.Connections", LogLevel.Debug);
-                      });
+                    if (useHttps)
+                    {
 
-                    webBuilder.UseStartup<Startup>();
+                        var certPassword = Environment.GetEnvironmentVariable("ASPNETCORE_Kestrel__Certificates__Development__Password");
+                        var certPath = Environment.GetEnvironmentVariable("ASPNETCORE_Kestrel__Certificates__Development__Path");
+
+                        // if not yet defined load config from usersecrets (development env only) or appsettings
+                        if (certPassword == null)
+                        {
+                            certPassword = configuration["Kestrel:Certificates:Default:Password"];
+                        }
+
+                        if (certPath == null)
+                        {
+                            certPath = configuration["Kestrel:Certificates:Default:Path"];
+                        }
+
+                        try
+                        {
+                            var certificate = new X509Certificate2(certPath, certPassword);
+
+                            // if password is wrong at this stage the attempts to use the cert will results in SSL Protocol Error
+
+                            var httpsConnectionAdapterOptions = new HttpsConnectionAdapterOptions()
+                            {
+                                ClientCertificateMode = ClientCertificateMode.NoCertificate,
+                                SslProtocols = System.Security.Authentication.SslProtocols.Tls12 | System.Security.Authentication.SslProtocols.Tls13,
+                                ServerCertificate = certificate,
+                            };
+
+                            var httpsPort = Convert.ToInt32(configuration["API:Service:HttpsPort"]);
+
+                            serverOptions.Listen(new System.Net.IPEndPoint(ipBinding, httpsPort), listenOptions =>
+                            {
+                                listenOptions.UseHttps(httpsConnectionAdapterOptions);
+                            });
+
+                        }
+                        catch (Exception exp)
+                        {
+                            // TODO: there is no logger yet, need to report this failure to main log once the log exists
+                            System.Diagnostics.Debug.WriteLine("Failed to load PFX certificate for application. Check service certificate config." + exp.ToString());
+                        }
+                    }
+                    else
+                    {
+                        var httpPort = Convert.ToInt32(configuration["API:Service:HttpPort"]);
+
+                        serverOptions.Listen(new System.Net.IPEndPoint(ipBinding, httpPort), listenOptions =>
+                        {
+                        });
+                    }
                 });
+
+                webBuilder.ConfigureLogging(logging =>
+                  {
+                      logging.AddFilter("Microsoft.AspNetCore.SignalR", LogLevel.Debug);
+                      logging.AddFilter("Microsoft.AspNetCore.Http.Connections", LogLevel.Debug);
+                  });
+
+                webBuilder.UseStartup<Startup>();
+            });
 
             return builder;
         }
--- src/Certify.Server/Certify.Service.Worker/Certify.Service.Worker/Worker.cs
+++ src/Certify.Server/Certify.Service.Worker/Certify.Service.Worker/Worker.cs
@@ -17,10 +17,33 @@ namespace Certify.Service.Worker
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            while (!stoppingToken.IsCancellationRequested)
+            // https://learn.microsoft.com/en-us/dotnet/core/extensions/windows-service?pivots=dotnet-7-0
+            try
             {
-                _logger.LogInformation("Certify Service Worker heartbeat: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000 * 60 * 60, stoppingToken);
+                while (!stoppingToken.IsCancellationRequested)
+                {
+                    _logger.LogInformation("Certify Service Worker heartbeat: {time}", DateTimeOffset.Now);
+                    await Task.Delay(1000 * 60 * 60, stoppingToken);
+                }
+            }
+            catch (TaskCanceledException)
+            {
+                // When the stopping token is canceled, for example, a call made from services.msc,
+                // we shouldn't exit with a non-zero exit code. In other words, this is expected...
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "{Message}", ex.Message);
+
+                // Terminates this process and returns an exit code to the operating system.
+                // This is required to avoid the 'BackgroundServiceExceptionBehavior', which
+                // performs one of two scenarios:
+                // 1. When set to "Ignore": will do nothing at all, errors cause zombie services.
+                // 2. When set to "StopHost": will cleanly stop the host, and log errors.
+                //
+                // In order for the Windows Service Management system to leverage configured
+                // recovery options, we need to terminate the process with a non-zero exit code.
+                Environment.Exit(1);
             }
         }
     }
