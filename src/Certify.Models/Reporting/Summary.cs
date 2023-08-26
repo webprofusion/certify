40200a85d04ec1325356e47e904547f0978e7863
webprofusion-chrisc <christopher.cook@webprofusion.com>
2023-08-26 09:58:45 +0800

    U/Core: implement summary query instead of counting all managed certs in the UI

---
+++ src/Certify.Models/Reporting/Summary.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Certify.Models;
+
+namespace Certify.Reporting
+{
+    public class Summary : BindableBase
+    {
+        public int Total { get; set; }
+        public int Healthy { get; set; }
+        public int Error { get; set; }
+        public int Warning { get; set; }
+        public int AwaitingUser { get; set; }
+        public int InvalidConfig { get; set; }
+
+        public int NoCertificate { get; set; }
+
+        public int TotalDomains { get; set; }
+    }
+}
