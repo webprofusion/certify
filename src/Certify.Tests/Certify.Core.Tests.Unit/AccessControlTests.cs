54f6a973ba74505e3afe563fadf7c86cf9a7ea48
webprofusion-chrisc <christopher.cook@webprofusion.com>
2023-09-22 16:30:04 +0800

    Access Control: WIP implement RBAC and storage

--- src/Certify.Tests/Certify.Core.Tests.Unit/AccessControlTests.cs
+++ src/Certify.Tests/Certify.Core.Tests.Unit/AccessControlTests.cs
@@ -1,57 +1,77 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Certify.Core.Management.Access;
 using Certify.Models;
+using Certify.Models.Config.AccessControl;
+using Certify.Providers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Serilog;
 
 namespace Certify.Core.Tests.Unit
 {
-
-    public class MemoryObjectStore : IObjectStore
+    public class MemoryObjectStore : IAccessControlStore
     {
-        private ConcurrentDictionary<string, object> _store = new ConcurrentDictionary<string, object>();
+        private ConcurrentDictionary<string, AccessStoreItem> _store = new ConcurrentDictionary<string, AccessStoreItem>();
 
-        public Task<T> Load<T>(string id)
+        public Task Add<T>(string itemType, AccessStoreItem item)
         {
-            if (_store.TryGetValue(id, out var value))
-            {
-                return Task.FromResult((T)value);
-            }
-            else
-            {
-                var empty = (T)Activator.CreateInstance(typeof(T));
-
-                return Task.FromResult(empty);
-            }
+            item.ItemType = itemType;
+            return Task.FromResult(_store.TryAdd(item.Id, item));
         }
 
-        public Task<bool> Save<T>(string id, object item)
+        public Task<bool> Delete<T>(string itemType, string id)
         {
-            _ = _store.AddOrUpdate(id, item, (key, oldVal) => item);
-            return Task.FromResult(true);
+            return Task.FromResult((_store.TryRemove(id, out _)));
+        }
+
+        public Task<List<T>> GetItems<T>(string itemType)
+        {
+            var items = _store.Values
+                    .Where((s => s.ItemType == itemType))
+                    .Select(s => (T)Convert.ChangeType(s, typeof(T)));
+
+            return Task.FromResult((items.ToList()));
+        }
+
+        public Task<T> Get<T>(string itemType, string id)
+        {
+            _store.TryGetValue(id, out var value);
+            return Task.FromResult((T)Convert.ChangeType(value, typeof(T)));
+        }
+
+        public Task Add<T>(string itemType, T item)
+        {
+            var o = item as AccessStoreItem;
+            o.ItemType = itemType;
+            return Task.FromResult(_store.TryAdd(o.Id, o));
+        }
+
+        public Task Update<T>(string itemType, T item)
+        {
+            var o = item as AccessStoreItem;
+            return Task.FromResult(_store.TryUpdate(o.Id, o, o));
         }
     }
 
     [TestClass]
     public class AccessControlTests
     {
-
         private List<SecurityPrinciple> GetTestSecurityPrinciples()
         {
-
-            return new List<SecurityPrinciple> {
-                new SecurityPrinciple {
+            return new List<SecurityPrinciple>
+            {
+                new SecurityPrinciple
+                {
                     Id = "admin_01",
                     Username = "admin",
                     Description = "Administrator account",
-                    Email="info@test.com", Password="ABCDEFG",
-                    PrincipleType= SecurityPrincipleType.User,
-                    SystemRoleIds=new List<string>{ StandardRoles.Administrator.Id
-    }
-},
+                    Email = "info@test.com",
+                    Password = "ABCDEFG",
+                    PrincipleType = SecurityPrincipleType.User
+                },
                 new SecurityPrinciple
                 {
                     Id = "domain_owner_01",
@@ -59,65 +79,34 @@ namespace Certify.Core.Tests.Unit
                     Description = "Example domain owner",
                     Email = "domains@test.com",
                     Password = "ABCDEFG",
-                    PrincipleType = SecurityPrincipleType.User,
-                    SystemRoleIds = new List<string> { StandardRoles.DomainOwner.Id }
+                    PrincipleType = SecurityPrincipleType.User
                 },
-                 new SecurityPrinciple
-                 {
-                     Id = "devops_user_01",
-                     Username = "devops_01",
-                     Description = "Example devops user",
-                     Email = "devops01@test.com",
-                     Password = "ABCDEFG",
-                     PrincipleType = SecurityPrincipleType.User,
-                     SystemRoleIds = new List<string> { StandardRoles.CertificateConsumer.Id, StandardRoles.DomainRequestor.Id }
-                 },
-                  new SecurityPrinciple
-                  {
-                      Id = "devops_app_01",
-                      Username = "devapp_01",
-                      Description = "Example devops app domain consumer",
-                      Email = "dev_app01@test.com",
-                      Password = "ABCDEFG",
-                      PrincipleType = SecurityPrincipleType.User,
-                      SystemRoleIds = new List<string> { StandardRoles.CertificateConsumer.Id }
-                  }
-            };
-        }
-
-        public List<ResourceProfile> GetTestResourceProfiles()
-        {
-            return new List<ResourceProfile> {
-                new ResourceProfile {
-                    ResourceType = ResourceTypes.System,
-                    AssignedRoles = new List<ResourceAssignedRole>{
-                        new ResourceAssignedRole{ RoleId=StandardRoles.Administrator.Id, PrincipleId = "admin_01" },
-                        new ResourceAssignedRole{ RoleId=StandardRoles.CertificateConsumer.Id, PrincipleId = "devops_user_01" },
-                        new ResourceAssignedRole{ RoleId=StandardRoles.DomainRequestor.Id, PrincipleId = "devops_user_01" }
-                    }
+                new SecurityPrinciple
+                {
+                    Id = "devops_user_01",
+                    Username = "devops_01",
+                    Description = "Example devops user",
+                    Email = "devops01@test.com",
+                    Password = "ABCDEFG",
+                    PrincipleType = SecurityPrincipleType.User
                 },
-                new ResourceProfile {
-                    ResourceType = ResourceTypes.Domain,
-                    Identifier = "example.com",
-                    AssignedRoles= new List<ResourceAssignedRole>{
-                        new ResourceAssignedRole{ RoleId=StandardRoles.CertificateConsumer.Id, PrincipleId = "devops_user_01" },
-                        new ResourceAssignedRole{ RoleId=StandardRoles.DomainRequestor.Id, PrincipleId = "devops_user_01" }
-                    }
+                new SecurityPrinciple
+                {
+                    Id = "devops_app_01",
+                    Username = "devapp_01",
+                    Description = "Example devops app domain consumer",
+                    Email = "dev_app01@test.com",
+                    Password = "ABCDEFG",
+                    PrincipleType = SecurityPrincipleType.User
                 },
-                  new ResourceProfile {
-                    ResourceType = ResourceTypes.Domain,
-                    Identifier = "www.example.com",
-                    AssignedRoles= new List<ResourceAssignedRole>{
-                        new ResourceAssignedRole{ RoleId=StandardRoles.CertificateConsumer.Id, PrincipleId = "devops_user_01" },
-                        new ResourceAssignedRole{ RoleId=StandardRoles.DomainRequestor.Id, PrincipleId = "devops_user_01" }
-                    }
-                },
-                new ResourceProfile {
-                    ResourceType = ResourceTypes.Domain,
-                    Identifier = "*.microsoft.com",
-                    AssignedRoles= new List<ResourceAssignedRole>{
-                        new ResourceAssignedRole{ RoleId=StandardRoles.CertificateConsumer.Id, PrincipleId = "devops_user_01" }
-                    }
+                    new SecurityPrinciple
+                {
+                    Id = "[test]",
+                    Username = "test administrator",
+                    Description = "Example test administrator used as context user during test",
+                    Email = "test_admin@test.com",
+                    Password = "ABCDEFG",
+                    PrincipleType = SecurityPrincipleType.User
                 }
             };
         }
@@ -126,8 +115,8 @@ namespace Certify.Core.Tests.Unit
         public async Task TestAccessControlChecks()
         {
             var log = new LoggerConfiguration()
-                   .WriteTo.Debug()
-                   .CreateLogger();
+                .WriteTo.Debug()
+                .CreateLogger();
 
             var loggy = new Loggy(log);
 
@@ -139,46 +128,90 @@ namespace Certify.Core.Tests.Unit
             var allPrinciples = GetTestSecurityPrinciples();
             foreach (var p in allPrinciples)
             {
-                _ = await access.AddSecurityPrinciple(p, contextUserId, bypassIntegrityCheck: true);
+                _ = await access.AddSecurityPrinciple(contextUserId, p, bypassIntegrityCheck: true);
             }
 
-            // assign resource roles per principle
-            var allResourceProfiles = GetTestResourceProfiles();
-            foreach (var r in allResourceProfiles)
+            // setup known actions
+            var actions = Policies.GetStandardResourceActions();
+
+            foreach (var action in actions)
             {
-                _ = await access.AddResourceProfile(r, contextUserId, bypassIntegrityCheck: true);
+                await access.AddAction(action);
+            }
+
+            // setup policies with actions
+
+            var policies = Policies.GetStandardPolicies();
+
+            // add policies to store
+            foreach (var r in policies)
+            {
+                _ = await access.AddResourcePolicy(contextUserId, r, bypassIntegrityCheck: true);
+            }
+
+            // setup roles with policies
+            var roles = await access.GetSystemRoles();
+
+            foreach (var r in roles)
+            {
+                // add roles and policy assignments to store
+                await access.AddRole(r);
+            }
+
+            // assign security principles to roles
+            var assignedRoles = new List<AssignedRole> {
+                // administrator
+                new AssignedRole{
+                    RoleId=StandardRoles.Administrator.Id,
+                    SecurityPrincipleId="admin_01"
+                },
+                // devops user in consumer role for a couple of specific domains
+                new AssignedRole{
+                    RoleId=StandardRoles.CertificateConsumer.Id,
+                    SecurityPrincipleId="devops_user_01",
+                    IncludedResources=new List<Resource>{
+                        new Resource{ ResourceType=ResourceTypes.Domain, Identifier="www.example.com" },
+                        new Resource{ ResourceType=ResourceTypes.Domain, Identifier="*.microsoft.com" }
+                    }
+                }
+            };
+
+            foreach (var r in assignedRoles)
+            {
+                // add roles and policy assignments to store
+                await access.AddAssignedRole(r);
             }
 
             // assert
 
-            var hasAccess = await access.IsPrincipleInRole("admin_01", StandardRoles.Administrator.Id, contextUserId);
+            var hasAccess = await access.IsPrincipleInRole(contextUserId, "admin_01", StandardRoles.Administrator.Id);
             Assert.IsTrue(hasAccess, "User should be in role");
 
-            hasAccess = await access.IsPrincipleInRole("admin_02", StandardRoles.Administrator.Id, contextUserId);
+            hasAccess = await access.IsPrincipleInRole(contextUserId, "admin_02", StandardRoles.Administrator.Id);
             Assert.IsFalse(hasAccess, "User should not be in role");
 
             // check user can consume a cert for a given domain 
-            var isAuthorised = await access.IsAuthorised("devops_user_01", StandardRoles.CertificateConsumer.Id, ResourceTypes.Domain, "www.example.com", contextUserId);
+            var isAuthorised = await access.IsAuthorised(contextUserId, "devops_user_01", StandardRoles.CertificateConsumer.Id, ResourceTypes.Domain, "certificate_download", "www.example.com");
             Assert.IsTrue(isAuthorised, "User should be a cert consumer for this domain");
 
             // check user can't consume a cert for a subdomain they haven't been granted
-            isAuthorised = await access.IsAuthorised("devops_user_01", StandardRoles.CertificateConsumer.Id, ResourceTypes.Domain, "secure.example.com", contextUserId);
+            isAuthorised = await access.IsAuthorised(contextUserId, "devops_user_01", StandardRoles.CertificateConsumer.Id, ResourceTypes.Domain, "certificate_download", "secure.example.com");
             Assert.IsFalse(isAuthorised, "User should not be a cert consumer for this domain");
 
             // check user can consume any subdomain via a granted wildcard
-            isAuthorised = await access.IsAuthorised("devops_user_01", StandardRoles.CertificateConsumer.Id, ResourceTypes.Domain, "random.microsoft.com", contextUserId);
+            isAuthorised = await access.IsAuthorised(contextUserId, "devops_user_01", StandardRoles.CertificateConsumer.Id, ResourceTypes.Domain, "certificate_download", "random.microsoft.com");
             Assert.IsTrue(isAuthorised, "User should be a cert consumer for this subdomain via wildcard");
 
             // check user can't consume a random wildcard
-            isAuthorised = await access.IsAuthorised("devops_user_01", StandardRoles.CertificateConsumer.Id, ResourceTypes.Domain, "*  lkjhasdf98862364", contextUserId);
+            isAuthorised = await access.IsAuthorised(contextUserId, "devops_user_01", StandardRoles.CertificateConsumer.Id, ResourceTypes.Domain, "certificate_download", "*  lkjhasdf98862364");
             Assert.IsFalse(isAuthorised, "User should not be a cert consumer for random wildcard");
 
             // check user can't consume a random wildcard
-            isAuthorised = await access.IsAuthorised("devops_user_01", StandardRoles.CertificateConsumer.Id, ResourceTypes.Domain, "  lkjhasdf98862364.*.microsoft.com", contextUserId);
+            isAuthorised = await access.IsAuthorised(contextUserId, "devops_user_01", StandardRoles.CertificateConsumer.Id, ResourceTypes.Domain, "certificate_download", "lkjhasdf98862364.*.microsoft.com");
             Assert.IsFalse(isAuthorised, "User should not be a cert consumer for random wildcard");
 
             // random user should not be authorised
-            isAuthorised = await access.IsAuthorised("randomuser", StandardRoles.CertificateConsumer.Id, ResourceTypes.Domain, "random.microsoft.com", contextUserId);
+            isAuthorised = await access.IsAuthorised(contextUserId, "randomuser", StandardRoles.CertificateConsumer.Id, ResourceTypes.Domain, "certificate_download", "random.microsoft.com");
             Assert.IsFalse(isAuthorised, "Unknown user should not be a cert consumer for this subdomain via wildcard");
         }
     }
