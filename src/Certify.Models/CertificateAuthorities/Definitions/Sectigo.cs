80b499a7a2ac19d0839442f4c50472811a2ac319
Justin Nelson <jrnelson90@gmail.com>
2023-10-13 12:04:05 -0700

    Add Sectigo Enterprise ACME CA to list of built in CAs
    https://github.com/webprofusion/certify-general/issues/19

--- src/Certify.Models/CertificateAuthorities/Definitions/Sectigo.cs
+++ src/Certify.Models/CertificateAuthorities/Definitions/Sectigo.cs
@@ -120,4 +120,44 @@ namespace Certify.CertificateAuthorities.Definitions
 
         }
     }
+
+    internal sealed class SectigoEnterprise
+    {
+        public static CertificateAuthority GetDefinition()
+        {
+            return new CertificateAuthority
+            {
+                Id = "sectigo-enterprise",
+                Title = "Sectigo.com (Enterprise certificates)",
+                Description = "Sectigo offer paid ACME services to their customers. see https://sectigo.com for more details",
+                APIType = CertAuthorityAPIType.ACME_V2.ToString(),
+                WebsiteUrl = "https://sectigo.com/resource-library/sectigos-acme-automation",
+                PrivacyPolicyUrl = "https://sectigo.com/privacy-policy/",
+                ProductionAPIEndpoint = "https://acme.enterprise.sectigo.com/",
+                StagingAPIEndpoint = string.Empty,
+                IsEnabled = true,
+                IsCustom = false,
+                SANLimit = 100,
+                StandardExpiryDays = 90,
+                RequiresEmailAddress = true,
+                RequiresExternalAccountBinding = true,
+                AllowInternalHostnames = true,
+                SupportsCachedValidations = true,
+                SupportedFeatures = new List<string>{
+                        CertAuthoritySupportedRequests.DOMAIN_SINGLE.ToString(),
+                        CertAuthoritySupportedRequests.DOMAIN_MULTIPLE_SAN.ToString(),
+                        CertAuthoritySupportedRequests.DOMAIN_WILDCARD.ToString()
+                    },
+                SupportedKeyTypes = new List<string>{
+                        StandardKeyTypes.RSA256_3072,
+                        StandardKeyTypes.RSA256_4096,
+                        StandardKeyTypes.ECDSA256,
+                        StandardKeyTypes.ECDSA384,
+                        StandardKeyTypes.ECDSA521
+                    },
+                EabInstructions = "Use the Sectigo administration portal to acquire EAB credentials for use with an ACME client, then create your account within the app and provide the EAB credentials."
+            };
+
+        }
+    }
 }
