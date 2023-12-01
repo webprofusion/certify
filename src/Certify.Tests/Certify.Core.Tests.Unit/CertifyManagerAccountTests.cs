907b275c4ee847d19a291bec8c0d90ed25fb753e
Justin Nelson <jrnelson90@gmail.com>
2023-11-30 21:56:06 -0800

    Changes to CertifyManagerAccountTests for Windows GitHub Actions runner

--- src/Certify.Tests/Certify.Core.Tests.Unit/CertifyManagerAccountTests.cs
+++ src/Certify.Tests/Certify.Core.Tests.Unit/CertifyManagerAccountTests.cs
@@ -90,12 +90,11 @@ namespace Certify.Core.Tests.Unit
                 // Create new volume for step-ca container
                 _stepVolume = new VolumeBuilder().WithName("step").Build();
                 await _stepVolume.CreateAsync();
-
                 // Create new step-ca container
                 _caContainer = new ContainerBuilder()
                     .WithName("step-ca")
                     // Set the image for the container to "smallstep/step-ca:latest".
-                    .WithImage("smallstep/step-ca:latest")
+                    .WithImage(_isWindows && Environment.GetEnvironmentVariable("GITHUB_WORKSPACE") != null ? "jrnelson90/step-ca-win:latest" : "smallstep/step-ca:latest")
                     .WithVolumeMount(_stepVolume, "/home/step")
                     // Bind port 9000 of the container to port 9000 on the host.
                     .WithPortBinding(_caPort)
