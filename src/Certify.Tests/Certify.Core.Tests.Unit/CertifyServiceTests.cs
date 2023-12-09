fc1efb60ac77d6e9d336a21848d90c2be0ee15dc
Justin Nelson <jrnelson90@gmail.com>
2023-12-09 01:06:09 -0800

    Add basic Unit Test for Certify.Service so it shows on 4.6.2 coverage

---
+++ src/Certify.Tests/Certify.Core.Tests.Unit/CertifyServiceTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Certify.Core.Tests.Unit
+{
+    [TestClass]
+    public class CertifyServiceTests
+    {
+        [TestMethod, Description("Validate that Service Program Main() does not start with bad args")]
+        public async Task TestProgramMainFails()
+        {
+            var exitCode = Certify.Service.Program.Main(null);
+
+            await Task.Delay(5000);
+
+            Assert.AreEqual(exitCode, 1067, "Unexpected exit code");
+        }
+    }
+}
