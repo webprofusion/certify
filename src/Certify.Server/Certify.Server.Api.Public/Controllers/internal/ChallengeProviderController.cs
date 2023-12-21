66fac613a996b8ae2c5db6179dc895e7752b48dd
webprofusion-chrisc <christopher.cook@webprofusion.com>
2023-12-21 17:22:42 +0800

    API: all controllers should be partial classes for extension with source gen

--- src/Certify.Server/Certify.Server.Api.Public/Controllers/internal/ChallengeProviderController.cs
+++ src/Certify.Server/Certify.Server.Api.Public/Controllers/internal/ChallengeProviderController.cs
@@ -11,7 +11,7 @@ namespace Certify.Server.Api.Public.Controllers
     /// </summary>
     [ApiController]
     [Route("internal/v1/[controller]")]
-    public class ChallengeProviderController : ControllerBase
+    public partial class ChallengeProviderController : ControllerBase
     {
 
         private readonly ILogger<ChallengeProviderController> _logger;
