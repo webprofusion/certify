0dbe39124817cf57870748a9bc538c70cded4b13
webprofusion-chrisc <christopher.cook@webprofusion.com>
2024-01-08 17:05:43 +0800

    Cleanup

--- src/Certify.Core/Management/CertifyManager/CertifyManager.cs
+++ src/Certify.Core/Management/CertifyManager/CertifyManager.cs
@@ -541,7 +541,7 @@ namespace Certify.Management
         private void Cleanup()
         {
             ManagedCertificateLog.DisposeLoggers();
-            if(_tc != null)
+            if (_tc != null)
             {
                 _tc.Dispose();
             }
