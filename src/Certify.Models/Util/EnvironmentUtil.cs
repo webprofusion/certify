3426b5e997a34e51b923229f9f550bdf82dedc57
webprofusion-chrisc <christopher.cook@webprofusion.com>
2024-02-22 17:40:34 +0800

    Standardise app data folder path creation/retrieval

--- src/Certify.Models/Util/EnvironmentUtil.cs
+++ src/Certify.Models/Util/EnvironmentUtil.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Runtime.InteropServices;
+using System.Security.AccessControl;
+using System.Security.Principal;
 
 namespace Certify.Models
 {
     public class EnvironmentUtil
     {
-
         public static string GetAppDataFolder(string? subFolder = null)
         {
             var parts = new List<string>()
@@ -15,16 +17,22 @@ namespace Certify.Models
                 Models.SharedConstants.APPDATASUBFOLDER
             };
 
-            if (subFolder != null)
-            {
-                parts.Add(subFolder);
-            }
-
             var path = Path.Combine(parts.ToArray());
 
             if (!Directory.Exists(path))
             {
                 Directory.CreateDirectory(path);
+            }
+
+            if (subFolder != null)
+            {
+                parts.Add(subFolder);
+                path = Path.Combine(parts.ToArray());
+
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
             }
 
             return path;
