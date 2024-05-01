9ec288d2aedb2fe9691f2828744a7f83304e04c9
webprofusion-chrisc <christopher.cook@webprofusion.com>
2024-05-01 17:18:27 +0800

    ARI: update CertID computation. Remove older draft of Update method

--- src/Certify.Tests/Certify.Core.Tests.Unit/Tests/MiscTests.cs
+++ src/Certify.Tests/Certify.Core.Tests.Unit/Tests/MiscTests.cs
@@ -61,5 +61,20 @@ namespace Certify.Core.Tests.Unit
                 Assert.Fail("NTP Time Difference Failed");
             }
         }
+#if NET7_0_OR_GREATER
+        [TestMethod, Description("Test ARI CertID encoding example")]
+        public void TestARICertIDEncoding()
+        {
+            // https://letsencrypt.org/2024/04/25/guide-to-integrating-ari-into-existing-acme-clients
+            var certAKIbytes = Convert.FromHexString("69:88:5B:6B:87:46:40:41:E1:B3:7B:84:7B:A0:AE:2C:DE:01:C8:D4".Replace(":",""));
+            var certSerialBytes= Convert.FromHexString("00:87:65:43:21".Replace(":",""));
+
+            var certId = Certify.Management.Util.ToUrlSafeBase64String(certAKIbytes) 
+                + "." 
+                + Certify.Management.Util.ToUrlSafeBase64String(certSerialBytes);
+
+           Assert.AreEqual("aYhba4dGQEHhs3uEe6CuLN4ByNQ.AIdlQyE", certId);
+        }
+#endif
     }
 }
