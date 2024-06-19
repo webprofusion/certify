910498cf96e8dca4a2b8c73e03d45d3b4c854edb
webprofusion-chrisc <christopher.cook@webprofusion.com>
2024-06-19 17:47:13 +0800

    Misc cleanup

--- src/Certify.Shared/Management/CertificateManager.cs
+++ src/Certify.Shared/Management/CertificateManager.cs
@@ -751,7 +751,7 @@ namespace Certify.Management
             }
 
             // if EC/CNG key may be under /keys
-            machineKeyPath = Path.Combine(appDataPath, "Microsoft", "Crypto", "Keys");
+            machineKeyPath = Path.Combine(new string[] { appDataPath, "Microsoft", "Crypto", "Keys" });
 
             // if EC/CNG key may be under /keys
             machineKeyPath = Path.Combine(new string[] { appDataPath, "Microsoft", "Crypto", "Keys" });
