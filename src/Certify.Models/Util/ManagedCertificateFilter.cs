92b5ec9b949738f86b4845dc2bd8386db27beeb5
webprofusion-chrisc <christopher.cook@webprofusion.com>
2024-08-05 14:18:44 +0800

    Cleanup

--- src/Certify.Models/Util/ManagedCertificateFilter.cs
+++ src/Certify.Models/Util/ManagedCertificateFilter.cs
@@ -1,18 +1,29 @@
-﻿#nullable disable
-namespace Certify.Models
+﻿namespace Certify.Models
 {
     public class ManagedCertificateFilter
     {
+        public enum SortMode
+        {
+            /// <summary>
+            /// Sort by Name, Ascending
+            /// </summary>
+            NAME_ASC,
+            /// <summary>
+            /// Sort by Date Last Renewed or Last Renewal Attempt if not yet renewed, Ascending
+            /// </summary>
+            RENEWAL_ASC
+        }
+
         public static readonly ManagedCertificateFilter ALL = new() { MaxResults = -1 };
 
         // optional filter on Id 
-        public string Id { get; set; }
+        public string? Id { get; set; }
 
         // optional filter on specific name
-        public string Name { get; set; }
+        public string? Name { get; set; }
 
         // optional keyword to filter name or domains
-        public string Keyword { get; set; }
+        public string? Keyword { get; set; }
 
         // filter results to just those sites which will be included in the next auto renewal
         public bool IncludeOnlyNextAutoRenew { get; set; }
@@ -20,13 +31,13 @@ namespace Certify.Models
         public int MaxResults { get; set; }
 
         // filter results to items with the given challenge type
-        public string ChallengeType { get; set; }
+        public string? ChallengeType { get; set; }
 
         // filter results to items with the given challenge provider (DNS helper API etc)
-        public string ChallengeProvider { get; set; }
+        public string? ChallengeProvider { get; set; }
 
         // filter results to items with the given challenge provider credentials (API key usage etc)
-        public string StoredCredentialKey { get; set; }
+        public string? StoredCredentialKey { get; set; }
 
         /// <summary>
         /// Optional page index for paging results
@@ -54,8 +65,10 @@ namespace Certify.Models
         public int? LastRenewalInfoCheckMins { get; set; }
 
         /// <summary>
-        /// Optonal description for the current set of filters (e.g. specific tests)
+        /// Optional description for the current set of filters (e.g. specific tests)
         /// </summary>
-        public string FilterDescription { get; set; }
+        public string FilterDescription { get; set; } = string.Empty;
+
+        public SortMode OrderBy { get; set; } = ManagedCertificateFilter.SortMode.NAME_ASC;
     }
 }
