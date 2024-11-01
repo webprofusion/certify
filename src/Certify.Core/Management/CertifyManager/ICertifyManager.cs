f023b13b70b9a35dac3564c0e1fb945664232cfa
Christopher Cook <christopher.cook@webprofusion.com>
2024-11-01 16:57:03 +0800

    API cleanup

--- src/Certify.Core/Management/CertifyManager/ICertifyManager.cs
+++ src/Certify.Core/Management/CertifyManager/ICertifyManager.cs
@@ -53,7 +53,7 @@ namespace Certify.Management
         Task<List<StatusMessage>> TestChallenge(ILog log, ManagedCertificate managedCertificate, bool isPreviewMode, IProgress<RequestProgressState> progress = null);
         Task<List<StatusMessage>> PerformChallengeCleanup(ILog log, ManagedCertificate managedCertificate, IProgress<RequestProgressState> progress = null);
         Task<List<ActionResult>> PerformServiceDiagnostics();
-        Task<List<DnsZone>> GetDnsProviderZones(string providerTypeId, string credentialsId);
+        Task<List<DnsZone>> GetDnsProviderZones(string providerTypeId, string credentialId);
         Task<ActionResult> UpdateCertificateAuthority(CertificateAuthority certificateAuthority);
         Task<List<CertificateAuthority>> GetCertificateAuthorities();
 
