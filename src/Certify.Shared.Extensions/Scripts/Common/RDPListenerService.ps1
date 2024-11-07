30d6a072c65ee99113bc2e2b749c1363b422071b
jing8956 <jing8956@outlook.com>
2024-11-08 02:56:10 +0800

    Fall back to the CIM cmdlet when WMIC is deprecated.

--- src/Certify.Shared.Extensions/Scripts/Common/RDPListenerService.ps1
+++ src/Certify.Shared.Extensions/Scripts/Common/RDPListenerService.ps1
@@ -7,4 +7,14 @@
 param($result)
 
 # Apply certificate
-wmic /namespace:\\root\cimv2\TerminalServices PATH Win32_TSGeneralSetting Set SSLCertificateSHA1Hash="$($result.ManagedItem.CertificateThumbprintHash)"
+if (Get-Command wmic -errorAction SilentlyContinue)
+{
+    # Beginning with Windows Server 2025, WMIC is available as a feature on demand.
+    wmic /namespace:\\root\cimv2\TerminalServices PATH Win32_TSGeneralSetting Set SSLCertificateSHA1Hash="$($result.ManagedItem.CertificateThumbprintHash)"
+}
+else
+{
+    # For new development, use the CIM cmdlets instead.
+    $instance = Get-CimInstance -ClassName Win32_TSGeneralSetting -Namespace root/cimv2/TerminalServices
+    Set-CimInstance -InputObject $instance -Property @{SSLCertificateSHA1Hash=$result.ManagedItem.CertificateThumbprintHash}
+}
