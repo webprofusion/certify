3c9976494a487b68f8b9388e4c6d70727110d003
Christopher Cook <christopher.cook@webprofusion.com>
2024-11-08 17:38:19 +0800

    Fix access control store deletes

--- src/Certify.Core/Management/CertifyManager/CertifyManager.cs
+++ src/Certify.Core/Management/CertifyManager/CertifyManager.cs
@@ -295,6 +295,7 @@ namespace Certify.Management
                         // default sqlite storage
                         _itemManager = new SQLiteManagedItemStore("", _serviceLog);
                         _credentialsManager = new SQLiteCredentialStore("", _serviceLog);
+                        _accessControl = new AccessControl(_serviceLog, new SQLiteAccessControlStore("", _serviceLog));
                     }
                     else
                     {
@@ -323,6 +324,7 @@ namespace Certify.Management
                 {
                     _itemManager = new SQLiteManagedItemStore("", _serviceLog);
                     _credentialsManager = new SQLiteCredentialStore("", _serviceLog);
+                    _accessControl = new AccessControl(_serviceLog, new SQLiteAccessControlStore("",_serviceLog));
                 }
 
                 // attempt to create and delete a test item
@@ -522,12 +524,6 @@ namespace Certify.Management
         private IAccessControl _accessControl;
         public Task<IAccessControl> GetCurrentAccessControl()
         {
-            if (_accessControl == null)
-            {
-                var store = new SQLiteAccessControlStore();
-                _accessControl = new AccessControl(_serviceLog, store);
-            }
-
             return Task.FromResult(_accessControl);
         }
     }
