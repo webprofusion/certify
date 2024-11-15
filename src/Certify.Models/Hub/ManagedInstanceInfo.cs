ce8da9ac3e18ef8bf344e623c6abe95828084ab1
Christopher Cook <christopher.cook@webprofusion.com>
2024-11-15 16:17:26 +0800

    Report managed instance os and client details

--- src/Certify.Models/Hub/ManagedInstanceInfo.cs
+++ src/Certify.Models/Hub/ManagedInstanceInfo.cs
@@ -8,6 +8,11 @@ namespace Certify.Models.Hub
     {
         public string InstanceId { get; set; } = string.Empty;
         public string Title { get; set; } = string.Empty;
+        public string OS { get; set; } = string.Empty;
+        public string OSVersion { get; set; } = string.Empty;
+        public string ClientName { get; set; } = string.Empty;
+        public string ClientVersion { get; set; } = string.Empty;
+
         public List<string> Tags { get; set; } = new List<string>();
         public DateTimeOffset LastReported { get; set; }
     }
