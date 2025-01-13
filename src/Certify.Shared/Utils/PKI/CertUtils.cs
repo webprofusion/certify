2e1e27f32d592e2a12985d570294aee71579c5ad
Christopher Cook <christopher.cook@webprofusion.com>
2025-01-13 18:28:04 +0800

    Core: on non-windows generate all common cert components, including PFX

--- src/Certify.Shared/Utils/PKI/CertUtils.cs
+++ src/Certify.Shared/Utils/PKI/CertUtils.cs
@@ -1,6 +1,7 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 using Certify.Management;
 using Org.BouncyCastle.Asn1.X509;
