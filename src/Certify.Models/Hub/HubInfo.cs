07dabbe22f1092637db19c43304f3373dfdbd948
Christopher Cook <christopher.cook@webprofusion.com>
2025-02-04 16:33:22 +0800

    Refactor and cleanup

--- src/Certify.Models/Hub/HubInfo.cs
+++ src/Certify.Models/Hub/HubInfo.cs
@@ -6,4 +6,13 @@
 
         public VersionInfo Version { get; set; }
     }
+
+    public class HubHealth
+    {
+        public string Status { get; set; }
+        public string Detail { get; set; }
+        public string Version { get; set; }
+        public bool ServiceAvailable { get; set; }
+        public object env { get; set; }
+    }
 }
