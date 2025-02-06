b7df5c60a1411e6412155c4ec0bdc7ec561d8928
Christopher Cook <christopher.cook@webprofusion.com>
2025-02-06 14:22:57 +0800

    Aspire: conditionally launch either combined hubservice or individual services

--- src/Certify.Aspire/Certify.Aspire.AppHost/Program.cs
+++ src/Certify.Aspire/Certify.Aspire.AppHost/Program.cs
@@ -1,7 +1,17 @@
 ﻿var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.Certify_Server_Hub_Api>("certifyserverhubapi");
+var useIndependentServices = false;
 
-builder.AddProject<Projects.Certify_Server_Core>("certifyservercore");
+if (useIndependentServices)
+{
+    builder.AddProject<Projects.Certify_Server_Hub_Api>("certifyserverhubapi");
+
+    builder.AddProject<Projects.Certify_Server_Core>("certifyservercore");
+}
+else
+{
+    // use combined hubservice
+    builder.AddProject<Projects.Certify_Server_HubService>("certify-server-hubservice");
+}
 
 builder.Build().Run();
