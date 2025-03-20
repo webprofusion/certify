7268ad3511103f522654d1945ad4cc90668a6d03
Christopher Cook <christopher.cook@webprofusion.com>
2025-03-20 12:34:29 +0800

    Misc cleanup

--- src/Certify.Models/Providers/ICertifyManager.cs
+++ src/Certify.Models/Providers/ICertifyManager.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Threading;
@@ -24,34 +24,34 @@ namespace Certify.Management
         Task<Version> GetServerTypeVersion(StandardServerTypes serverType);
         Task<List<ActionStep>> RunServerDiagnostics(StandardServerTypes serverType, string siteId);
         Task<ManagedCertificate> GetManagedCertificate(string id);
-        Task<List<ManagedCertificate>> GetManagedCertificates(ManagedCertificateFilter filter = null);
-        Task<ManagedCertificateSearchResult> GetManagedCertificateResults(ManagedCertificateFilter filter = null);
-        Task<Certify.Models.Reporting.StatusSummary> GetManagedCertificateSummary(ManagedCertificateFilter filter = null);
+        Task<List<ManagedCertificate>> GetManagedCertificates(ManagedCertificateFilter? filter = null);
+        Task<ManagedCertificateSearchResult> GetManagedCertificateResults(ManagedCertificateFilter? filter = null);
+        Task<Certify.Models.Reporting.StatusSummary> GetManagedCertificateSummary(ManagedCertificateFilter? filter = null);
         Task<ManagedCertificate> UpdateManagedCertificate(ManagedCertificate site);
         Task<ActionResult> DeleteManagedCertificate(string id);
         Task<ImportExportPackage> PerformExport(ExportRequest exportRequest);
         Task<List<ActionStep>> PerformImport(ImportRequest importRequest);
-        Task<List<SimpleAuthorizationChallengeItem>> GetCurrentChallengeResponses(string challengeType, string key = null);
+        Task<List<SimpleAuthorizationChallengeItem>> GetCurrentChallengeResponses(string challengeType, string? key = null);
 
         Task<List<AccountDetails>> GetAccountRegistrations();
         Task<ActionResult> AddAccount(ContactRegistration reg);
         Task<ActionResult> UpdateAccountContact(string storageKey, ContactRegistration contact);
         Task<ActionResult> RemoveAccount(string storageKey, bool includeAccountDeactivation = false);
-        Task<ActionResult<AccountDetails>> ChangeAccountKey(string storageKey, string newKeyPEM = null);
+        Task<ActionResult<AccountDetails>> ChangeAccountKey(string storageKey, string? newKeyPEM = null);
 
-        Task<List<StatusMessage>> TestChallenge(ILog log, ManagedCertificate managedCertificate, bool isPreviewMode, IProgress<RequestProgressState> progress = null);
-        Task<List<StatusMessage>> PerformChallengeCleanup(ILog log, ManagedCertificate managedCertificate, IProgress<RequestProgressState> progress = null);
+        Task<List<StatusMessage>> TestChallenge(ILog log, ManagedCertificate managedCertificate, bool isPreviewMode, IProgress<RequestProgressState>? progress = null);
+        Task<List<StatusMessage>> PerformChallengeCleanup(ILog log, ManagedCertificate managedCertificate, IProgress<RequestProgressState>? progress = null);
         Task<List<ActionResult>> PerformServiceDiagnostics();
         Task<List<DnsZone>> GetDnsProviderZones(string providerTypeId, string credentialId);
         Task<ActionResult> UpdateCertificateAuthority(CertificateAuthority certificateAuthority);
         Task<List<CertificateAuthority>> GetCertificateAuthorities();
         Task<StatusMessage> RevokeCertificate(ILog log, ManagedCertificate managedCertificate);
-        Task<CertificateRequestResult> PerformDummyCertificateRequest(ManagedCertificate managedCertificate, IProgress<RequestProgressState> progress = null);
+        Task<CertificateRequestResult> PerformDummyCertificateRequest(ManagedCertificate managedCertificate, IProgress<RequestProgressState>? progress = null);
         Task<ActionResult> RemoveCertificateAuthority(string id);
-        Task<List<SiteInfo>> GetPrimaryWebSites(StandardServerTypes serverType, bool ignoreStoppedSites, string itemId = null);
-        Task<List<CertificateRequestResult>> RedeployManagedCertificates(ManagedCertificateFilter filter, IProgress<RequestProgressState> progress = null, bool isPreviewOnly = false, bool includeDeploymentTasks = false);
-        Task<CertificateRequestResult> DeployCertificate(ManagedCertificate managedCertificate, IProgress<RequestProgressState> progress = null, bool isPreviewOnly = false, bool includeDeploymentTasks = false);
-        Task<CertificateRequestResult> PerformCertificateRequest(ILog log, ManagedCertificate managedCertificate, IProgress<RequestProgressState> progress = null, bool resumePaused = false, bool skipRequest = false, bool failOnSkip = false, bool skipTasks = false, bool isInteractive = false, string reason = null);
+        Task<List<SiteInfo>> GetPrimaryWebSites(StandardServerTypes serverType, bool ignoreStoppedSites, string? itemId = null);
+        Task<List<CertificateRequestResult>> RedeployManagedCertificates(ManagedCertificateFilter filter, IProgress<RequestProgressState>? progress = null, bool isPreviewOnly = false, bool includeDeploymentTasks = false);
+        Task<CertificateRequestResult> DeployCertificate(ManagedCertificate managedCertificate, IProgress<RequestProgressState>? progress = null, bool isPreviewOnly = false, bool includeDeploymentTasks = false);
+        Task<CertificateRequestResult> PerformCertificateRequest(ILog log, ManagedCertificate managedCertificate, IProgress<RequestProgressState>? progress = null, bool resumePaused = false, bool skipRequest = false, bool failOnSkip = false, bool skipTasks = false, bool isInteractive = false, string? reason = null);
         Task<List<DomainOption>> GetDomainOptionsFromSite(StandardServerTypes serverType, string siteId);
 
         Task<List<CertificateRequestResult>> PerformRenewAll(RenewalSettings settings, CancellationToken cancellationToken, ConcurrentDictionary<string, Progress<RequestProgressState>> progressTrackers = null);
@@ -60,7 +60,7 @@ namespace Certify.Management
 
         Task<bool> PerformDailyMaintenanceTasks();
         Task PerformCertificateCleanup();
-        Task<List<ActionResult>> PerformCertificateMaintenanceTasks(string managedItemId = null);
+        Task<List<ActionResult>> PerformCertificateMaintenanceTasks(string? managedItemId = null);
         Task<List<ActionStep>> GeneratePreview(ManagedCertificate item);
         void ReportProgress(IProgress<RequestProgressState> progress, RequestProgressState state, bool logThisEvent = true);
         Task<List<ActionStep>> PerformDeploymentTask(ILog log, string managedCertificateId, string taskId, bool isPreviewOnly, bool skipDeferredTasks, bool forceTaskExecution);
