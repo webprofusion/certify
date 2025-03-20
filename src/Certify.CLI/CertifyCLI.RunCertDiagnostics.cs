1168c379c6f891c2919c5352851069c85c0260b2
Christopher Cook <christopher.cook@webprofusion.com>
2025-03-20 12:33:24 +0800

    CLI: cleanup project, remove parts of diagnostics to trim dependencies

--- src/Certify.CLI/CertifyCLI.RunCertDiagnostics.cs
+++ src/Certify.CLI/CertifyCLI.RunCertDiagnostics.cs
@@ -2,11 +2,8 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Runtime.InteropServices;
 using System.Threading.Tasks;
-using Certify.Management;
 using Certify.Models;
-using Microsoft.Extensions.Logging;
 
 namespace Certify.CLI
 {
@@ -19,6 +16,7 @@ namespace Certify.CLI
         /// <param name="forceAutoDeploy">Change all deployment modes to Auto</param>
         public async Task RunCertDiagnostics(bool autoFix = false, bool forceAutoDeploy = false, bool includeOcspCheck = true)
         {
+            // TODO: this should all move to the core service and be called via the client API
             static string stripNonNumericFromString(string input)
             {
                 return new string(input.Where(c => char.IsDigit(c)).ToArray());
@@ -61,7 +59,6 @@ namespace Certify.CLI
             Console.WriteLine("Running cert diagnostics..");
 
             var countSiteIdsFixed = 0;
-            var countBindingRedeployments = 0;
             var totalTime = Stopwatch.StartNew();
             var itemTiming = Stopwatch.StartNew();
 
@@ -70,20 +67,11 @@ namespace Certify.CLI
 
                 itemTiming.Restart();
 
-                var redeployRequired = false;
-
-                if (autoFix)
-                {
-                    redeployRequired = true;
-                }
-
                 if ((site.GroupId != site.ServerSiteId) || !isNumeric(site.ServerSiteId))
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine("\t WARNING: managed cert has invalid ServerSiteID: " + site.Name);
                     Console.ForegroundColor = ConsoleColor.White;
-
-                    redeployRequired = true;
 
                     if (autoFix)
                     {
@@ -93,7 +81,7 @@ namespace Certify.CLI
                         //update managed site
                         Console.WriteLine("\t Auto fixing managed cert ServerSiteID: " + site.Name);
 
-                        var update = await _certifyClient.UpdateManagedCertificate(site);
+                        await _certifyClient.UpdateManagedCertificate(site);
 
                         countSiteIdsFixed++;
                     }
@@ -101,120 +89,15 @@ namespace Certify.CLI
 
                 if (autoFix && forceAutoDeploy)
                 {
-                    redeployRequired = true;
-
                     if (site.RequestConfig.DeploymentSiteOption != DeploymentOption.Auto && site.RequestConfig.DeploymentSiteOption != DeploymentOption.AllSites)
                     {
                         Console.WriteLine("\t Auto fixing managed cert deployment mode: " + site.Name);
                         site.RequestConfig.DeploymentSiteOption = DeploymentOption.Auto;
 
-                        var update = await _certifyClient.UpdateManagedCertificate(site);
+                        await _certifyClient.UpdateManagedCertificate(site);
                     }
                 }
 
-#if !NET9_0_OR_GREATER
-                if (!string.IsNullOrEmpty(site.CertificatePath) && System.IO.File.Exists(site.CertificatePath))
-                {
-                    Console.WriteLine($"{site.Name}");
-                    var fileCert = CertificateManager.LoadCertificate(site.CertificatePath);
-
-                    if (fileCert != null)
-                    {
-                        try
-                        {
-                            var storedCert = CertificateManager.GetCertificateByThumbprint(site.CertificateThumbprintHash);
-                            if (storedCert != null)
-                            {
-                                // cert in store, check permissions
-                                Console.WriteLine($"Stored cert :: " + storedCert.FriendlyName);
-                                Console.WriteLine($"Signature Algorithm :: " + storedCert.SignatureAlgorithm.FriendlyName);
-
-                                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-                                {
-                                    var access = CertificateManager.GetUserAccessInfoForCertificatePrivateKey(storedCert);
-                                    foreach (System.Security.AccessControl.AuthorizationRule a in access.GetAccessRules(true, false, typeof(System.Security.Principal.NTAccount)))
-                                    {
-                                        Console.WriteLine("\t Access: " + a.IdentityReference.Value.ToString());
-                                    }
-                                }
-
-                            }
-
-                            if (includeOcspCheck)
-                            {
-                                var chainResults = CertificateManager.CheckCertChain(fileCert);
-
-                                foreach (var result in chainResults)
-                                {
-                                    Console.WriteLine($"\t Cert Ocsp Status Check: {fileCert.Subject} " + result);
-                                }
-
-                                var ocspCheck = await CertificateManager.CheckOcspRevokedStatus(site.CertificatePath, "");
-                                Console.ForegroundColor = ConsoleColor.White;
-
-                                if (ocspCheck == Models.Certify.Models.CertificateStatusType.Revoked || ocspCheck == Models.Certify.Models.CertificateStatusType.Expired)
-                                {
-                                    Console.ForegroundColor = ConsoleColor.Red;
-                                    Console.WriteLine($"\t Ocsp Status Check: {fileCert.Subject} " + ocspCheck);
-                                    Console.ForegroundColor = ConsoleColor.White;
-                                }
-                                else
-                                {
-                                    Console.WriteLine($"\t Ocsp Status Check: {fileCert.Subject} " + ocspCheck);
-                                }
-                            }
-
-                            // re-deploy certificate if possible
-                            if (redeployRequired && autoFix)
-                            {
-
-                                //re-apply current certificate file to store and bindings
-                                if (!string.IsNullOrEmpty(site.CertificateThumbprintHash))
-                                {
-                                    var result = await _certifyClient.ReapplyCertificateBindings(site.Id, false, false);
-
-                                    countBindingRedeployments++;
-
-                                    if (!result.IsSuccess)
-                                    {
-                                        Console.ForegroundColor = ConsoleColor.Red;
-                                        Console.WriteLine("\t Error: Failed to re-applying certificate bindings:" + site.Name);
-                                        Console.ForegroundColor = ConsoleColor.White;
-                                    }
-                                    else
-                                    {
-                                        Console.ForegroundColor = ConsoleColor.Green;
-                                        Console.WriteLine("\t Info: re-applied certificate bindings:" + site.Name);
-                                        Console.ForegroundColor = ConsoleColor.White;
-                                    }
-
-                                    System.Threading.Thread.Sleep(500);
-                                }
-                                else
-                                {
-
-                                    Console.ForegroundColor = ConsoleColor.DarkYellow;
-                                    Console.WriteLine($"Warning: {site.Name} :: No certificate information, bindings cannot be redeployed");
-                                    Console.ForegroundColor = ConsoleColor.White;
-
-                                }
-                            }
-                        }
-                        catch (Exception exp)
-                        {
-                            Console.WriteLine(exp.ToString());
-                        }
-                    }
-                    else
-                    {
-                        //Console.WriteLine($"{site.Name} certificate file does not exist: {site.CertificatePath}");
-                        if (redeployRequired)
-                        {
-                            Console.WriteLine($"{site.Name} has no current certificate and requires manual verification/redeploy of cert.");
-                        }
-                    }
-                }
-#endif
                 Debug.WriteLine($"Item update took {itemTiming.Elapsed.TotalSeconds}s");
             }
 
@@ -273,50 +156,9 @@ namespace Certify.CLI
                 System.Console.WriteLine(url);
             }
 
-            if (autoFix)
-            {
-                System.Console.WriteLine("Auto fixing:");
-
-                // TODO: move this into certify manager and use client to call into service, removing dependency on certify core lib
-                var c = new CertifyManager();
-                await c.Init();
-
-                var logger = new Loggy(LoggerFactory.Create(builder => builder.AddDebug()).CreateLogger<CertifyCLI>());
-
-                foreach (var url in orderUrls)
-                {
-
-                    System.Console.WriteLine("Checking Pending Challenges for " + url);
-
-                    var dummyManagedCert = (new ManagedCertificate { CurrentOrderUri = url, UseStagingMode = false });
-                    // get 
-                    var caAccount = await c.GetAccountDetails(dummyManagedCert);
-                    var acmeClient = await c.GetACMEProvider(dummyManagedCert, caAccount);
-
-                    var pendingOrder = await acmeClient.BeginCertificateOrder(logger, dummyManagedCert, resumeExistingOrder: true);
-
-                    foreach (var auth in pendingOrder.Authorizations)
-                    {
-                        try
-                        {
-                            if (!auth.IsFailure && !auth.IsValidated)
-                            {
-                                System.Console.WriteLine("Submitting challenge for validation " + auth.Identifier);
-                                auth.AttemptedChallenge = auth.Challenges.FirstOrDefault(c => c.ChallengeType == SupportedChallengeTypes.CHALLENGE_TYPE_HTTP || c.ChallengeType == SupportedChallengeTypes.CHALLENGE_TYPE_DNS);
-                                await acmeClient.SubmitChallenge(logger, auth.AttemptedChallenge.ChallengeType, auth);
-                            }
-                        }
-                        catch (Exception)
-                        {
-                            System.Console.WriteLine("Failed to complete pending authz for  " + url);
-
-                        }
-
-                        await Task.Delay(250);
-                    }
-                }
-            }
+            await Task.CompletedTask;
         }
+
         public async Task RunCertMaintenanceTasks(string[] args)
         {
             System.Console.WriteLine("Performing managed certificate maintenance tasks..");
