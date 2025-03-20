3d5ad1f57638c994925ce889af4456f404f0e215
Christopher Cook <christopher.cook@webprofusion.com>
2025-03-20 12:29:39 +0800

    Webhook etc move basic model to Certify.Models, rearrange

--- src/Certify.CLI/CertifyCLI.ImportCSV.cs
+++ src/Certify.CLI/CertifyCLI.ImportCSV.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Certify.Models;
-using Certify.Shared.Utils;
+using Certify.Models.Utils;
 
 namespace Certify.CLI
 {
---
+++ src/Certify.Models/Util/WebhookConfig.cs
@@ -0,0 +1,30 @@
+﻿namespace Certify.Models.Utils
+{
+    public class WebhookConfig
+    {
+        /// <summary>
+        /// The trigger for the webhook (None, Success, Error) 
+        /// </summary>
+        public string Trigger { get; set; } = "None";
+
+        /// <summary>
+        /// The http method for the webhook request 
+        /// </summary>
+        public string? Method { get; set; }
+
+        /// <summary>
+        /// The http url for the webhook request 
+        /// </summary>
+        public string? Url { get; set; }
+
+        /// <summary>
+        /// The http content type header for the webhook request 
+        /// </summary>
+        public string? ContentType { get; set; }
+
+        /// <summary>
+        /// The http body template for the webhook request 
+        /// </summary>
+        public string? ContentBody { get; set; }
+    }
+}
