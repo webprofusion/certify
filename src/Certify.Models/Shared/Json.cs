a6bcd7069aea0b3f2b168ba5c61d61ff5c0bb9f5
Christopher Cook <christopher.cook@webprofusion.com>
2025-03-27 15:32:14 +0800

    Use default case-insensitive json deserialization. Cleanup

---
+++ src/Certify.Models/Shared/Json.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json;
+
+namespace Certify.Shared
+{
+    public class JsonOptions
+    {
+        public static JsonSerializerOptions DefaultJsonSerializerOptions = new() { PropertyNameCaseInsensitive = true };
+    }
+}
