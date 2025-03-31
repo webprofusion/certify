82583b70358bb0152788a34903e5520bfdb7de78
Christopher Cook <christopher.cook@webprofusion.com>
2025-03-31 19:10:52 +0800

    WIP: implement item tags

---
+++ src/Certify.Models/Hub/ItemTag.cs
@@ -0,0 +1,27 @@
+﻿namespace Certify.Models.Hub
+{
+    /// <summary>
+    /// Generic tagging for different configuration items
+    /// </summary>
+    public class ItemTag : ConfigurationStoreItem
+    {
+        public ItemTag(string itemId, string itemType, string tag, string? value)
+        {
+            TaggedItemId = itemId;
+            TaggedItemType = itemType;
+            Tag = tag;
+            Value = value;
+        }
+
+        public string TaggedItemId { get; set; }
+        public string TaggedItemType { get; set; }
+        public string Tag { get; set; }
+        public string? Value { get; set; }
+    }
+
+    public class TagDefinition : ConfigurationStoreItem
+    {
+        public string Tag { get; set; } = default!;
+        public string? ParentTag { get; set; }
+    }
+}
