76af6607a764d2ef8624deed197d1ad68402523e
Christopher Cook <christopher.cook@webprofusion.com>
2025-06-03 19:07:27 +0800

    Add faulted svc config info in challenge server

--- src/Certify.Shared/Utils/HttpChallengeServer.cs
+++ src/Certify.Shared/Utils/HttpChallengeServer.cs
@@ -146,7 +146,7 @@ namespace Certify.Core.Management.Challenges
 
                     Log($"Http Challenge Server Started: {uriPrefix}", true);
                     Log($"Control Key: {_controlKey}: Check Key: {_checkKey}");
-                    Log($"Using API: {_baseUri}");
+                    Log($"Using Internal API: {_baseUri}");
 
                     _ = Task.Run(ServerTask);
 
