ff9f558ad30f69e3c3ebe9a3d38ddf200a1980cd
Christopher Cook <christopher.cook@webprofusion.com>
2025-08-06 16:47:22 +0800

    CLI: fix json serialization for stored credential list

--- src/Certify.CLI/CertifyCLI.StoredCredentials.cs
+++ src/Certify.CLI/CertifyCLI.StoredCredentials.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using Certify.Models.Config;
 using Newtonsoft.Json;
@@ -8,6 +11,37 @@ namespace Certify.CLI
 {
     public partial class CertifyCLI
     {
+
+        /// <summary>
+        /// Json Serializer Context hints for StoredCredResult to enable source generation for json serialization.
+        /// </summary>
+        [JsonSerializable(typeof(StoredCredResult))]
+        [JsonSerializable(typeof(StoredCredOperationResult))]
+        [JsonSerializable(typeof(IEnumerable<StoredCredResult>))]
+        public partial class StoredCredResultContext : JsonSerializerContext
+        {
+        }
+
+        JsonSerializerOptions _credSerializationOptions = new()
+        {
+            TypeInfoResolver = StoredCredResultContext.Default,
+            WriteIndented = true
+        };
+
+        public partial class StoredCredResult
+        {
+            public string Title { get; set; } = string.Empty;
+            public string StorageKey { get; set; } = string.Empty;
+            public string ProviderType { get; set; } = string.Empty;
+            public DateTimeOffset DateCreated { get; set; }
+            public DateTimeOffset? DateExpiry { get; set; }
+        }
+        public partial class StoredCredOperationResult
+        {
+            public string Status { get; set; } = string.Empty;
+            public string Message { get; set; } = string.Empty;
+            public string StorageKey { get; set; } = string.Empty;
+        }
 
         internal async Task UpdateStoredCredential(string[] args)
         {
@@ -37,13 +71,13 @@ namespace Certify.CLI
                 if (result != null)
                 {
 
-                    var resultObject = new { Status = "OK", Message = "Credential updated", StorageKey = result?.StorageKey };
+                    var resultObject = new StoredCredOperationResult { Status = "OK", Message = "Credential updated", StorageKey = result?.StorageKey };
                     var output = JsonConvert.SerializeObject(resultObject, Formatting.Indented);
                     Console.WriteLine(output);
                 }
                 else
                 {
-                    var resultObject = new { Status = "Error", Message = "Credential update failed" };
+                    var resultObject = new StoredCredOperationResult { Status = "Error", Message = "Credential update failed" };
                     var output = JsonConvert.SerializeObject(resultObject, Formatting.Indented);
                     Console.WriteLine(output);
                 }
@@ -56,11 +90,28 @@ namespace Certify.CLI
 
         internal async Task ListStoredCredentials(string[] args)
         {
-            var result = await _certifyClient.GetCredentials();
+            try
+            {
+                var result = await _certifyClient.GetCredentials();
 
-            var output = JsonConvert.SerializeObject(result.Select(s => new { s.Title, s.StorageKey, s.ProviderType, s.DateCreated }), Formatting.Indented);
+                var list = result.Select(s => new StoredCredResult
+                {
+                    Title = s.Title ?? "N/A",
+                    StorageKey = s.StorageKey ?? "N/A",
+                    ProviderType = s.ProviderType ?? "N/A",
+                    DateCreated = s.DateCreated,
+                    DateExpiry = s.DateExpiry
+                });
 
-            Console.WriteLine(output);
+                var output = System.Text.Json.JsonSerializer.Serialize(list, options: _credSerializationOptions);
+
+                Console.WriteLine(output);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"ERROR: {ex.Message}");
+                Console.WriteLine($"STACK: {ex.StackTrace}");
+            }
         }
         private void WriteOutput(object resultObject)
         {
