6589bcb3efeede3e0682eda8d9fb736ae7e73474
Christopher Cook <christopher.cook@webprofusion.com>
2025-10-15 14:39:42 +0800

    Deprecate Buypass

--- src/Certify.Models/CertificateAuthorities/Definitions/BuyPass.cs
+++ src/Certify.Models/CertificateAuthorities/Definitions/BuyPass.cs
@@ -10,8 +10,8 @@ namespace Certify.CertificateAuthorities.Definitions
             return new CertificateAuthority
             {
                 Id = "buypass.com",
-                Title = "Buypass Go SSL",
-                Description = "Buypass Go SSL is a free SSL certificate service from Buypass CA using the Buypass ACME API. Certificates are valid for 180 days and can contain up to 5 domains per certificate (wildcards are not available)",
+                Title = "Buypass Go SSL (deprecated)",
+                Description = "Buypass Go SSL previously offered free ACME services which have been terminated since October 2025",
                 APIType = CertAuthorityAPIType.ACME_V2.ToString(),
                 WebsiteUrl = "https://www.buypass.com/",
                 PrivacyPolicyUrl = "https://www.buypass.com/about-buypass/privacy-policy",
