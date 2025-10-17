32b8f87c8d5f523e98c5e698c3130f645d007581
Christopher Cook <christopher.cook@webprofusion.com>
2025-10-17 11:50:14 +0800

    Add service version to log

--- src/Certify.Core/Management/CertifyManager/CertifyManager.cs
+++ src/Certify.Core/Management/CertifyManager/CertifyManager.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.IO;
@@ -121,6 +121,8 @@ namespace Certify.Management
             SettingsManager.LoadAppSettings();
 
             InitLogging(_serverConfig);
+
+            _serviceLog?.Information($"Certify Manager: {Util.GetAppVersion()}");
 
             Util.SetSupportedTLSVersions();
 
